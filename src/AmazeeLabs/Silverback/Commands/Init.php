d825fae727578db38cab40af7d30ea52f8f13031
Philipp Melab <philipp.melab@gmail.com>
2018-10-29 10:40:09 +0100

    Update Init.php

--- src/AmazeeLabs/Silverback/Commands/Init.php
+++ src/AmazeeLabs/Silverback/Commands/Init.php
@@ -124,7 +124,7 @@ class Init extends SilverbackCommand {
       "cd tests && npm install && CYPRESS_TAGS=@COMPLETED cypress run",
     ];
     $composerJson['extra']['enable-patching'] = TRUE;
-    file_put_contents($this->rootDirectory . '/composer.json', json_encode($composerJson, JSON_PRETTY_PRINT || JSON_UNESCAPED_SLASHES));
+    file_put_contents($this->rootDirectory . '/composer.json', json_encode($composerJson, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES));
   }
 
 }
