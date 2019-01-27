4a155399ee08a55fe8725baf5ddbea894640f783
Philipp Melab <philipp.melab@gmail.com>
2019-01-27 07:49:10 +0100

    Added some common cypress commands.

--- assets/tests/cypress/support/commands.js
+++ assets/tests/cypress/support/commands.js
@@ -23,3 +23,58 @@
 //
 // -- This is will overwrite an existing command --
 // Cypress.Commands.overwrite("visit", (originalFn, url, options) => { ... })
+
+/**
+ * Execute a sequence of commands and take a a named snapshot afterwards.
+ *
+ * Next time time the snapshot is loaded and the commands are skipped.
+ *
+ * Snapshot is automatically invalidated if the install configuration changes.
+ */
+Cypress.Commands.add('prepareSnapshot', (name, setup) => {
+  cy.exec(`cd .. && ./vendor/bin/silverback restore ${name}`,{failOnNonZeroExit: false} ).then((result) => {
+    if (result.code !== 0) {
+      setup();
+      cy.exec(`cd .. && ./vendor/bin/silverback snapshot ${name}`);
+    }
+  });
+});
+
+/**
+ * Hide the admin toolbar for the current page visit.
+ *
+ * Sometimes elements are inaccessible to cypress because they are hidden behind the toolbar.
+ */
+Cypress.Commands.add('hideAdminToolbar', () => {
+  cy.get('body > .toolbar').then( toolbar => toolbar.hide());
+});
+
+/**
+ * Make sure the admin toolbar is visible again.
+ */
+Cypress.Commands.add('showAdminToolbar', () => {
+  cy.get('body > .toolbar').then( toolbar => toolbar.show());
+});
+
+/**
+ * Retrieve an input element by searching for it's label.
+ */
+Cypress.Commands.add('inputByLabel', (label) => {
+  return cy.get('label').contains(label).then((result) => {
+    return cy.get('#' + result.attr('for'));
+  });
+});
+
+/**
+ * Retrieve an input element by it's value.
+ */
+Cypress.Commands.add('inputByValue', (value) => {
+  return cy.get(`input[value="${value}"]`);
+});
+
+/**
+ * Execute a drush command.
+ */
+Cypress.Commands.add('drush', command => {
+  return cy.exec(`cd .. && drush ${command}`);
+});
