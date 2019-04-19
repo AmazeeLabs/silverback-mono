a30107801e1e55ceb7362af1024d1f760efcc285
Philipp Melab <philipp.melab@gmail.com>
2019-04-19 08:39:51 +0200

    Set a default private file path.

--- assets/web/sites/default/settings.php
+++ assets/web/sites/default/settings.php
@@ -534,7 +534,7 @@ if ($settings['hash_salt']) {
  * See https://www.drupal.org/documentation/modules/file for more information
  * about securing private files.
  */
-# $settings['file_private_path'] = '';
+$settings['file_private_path'] = 'sites/default/files/private';
 
 /**
  * Session write interval:
