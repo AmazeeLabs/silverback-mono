7259cd8399366326f67214a2b99b27287d5ba490
John Albin Wilkins <virtually.johnalbin@gmail.com>
2019-11-01 15:43:17 +0800

    WIP: Improve storybook config (#97)
    * Self-document imported function from twig-drupal-filters
    
    * Import storybook stories from anywhere in storybook folder

--- assets/storybook/.storybook/config.js
+++ assets/storybook/.storybook/config.js
@@ -6,20 +6,18 @@ import { addDecorator, addParameters, configure } from '@storybook/html';
 import { withA11y } from '@storybook/addon-a11y';
 
 import Twig from 'twig';
-import twigDrupal from 'twig-drupal-filters';
+import addDrupalFilters from 'twig-drupal-filters';
 
 // Import styles
 import '../styles.css';
 
 // Add the filters to Drupal.
-twigDrupal(Twig);
+addDrupalFilters(Twig);
 
 // Automatically import all files ending in *.stories.js
-const twig = require.context('../twig', true, /\.stories\.(ts|js)$/);
-const editor = require.context('../editor', true, /\.stories\.(ts|js)$/);
+const stories = require.context('../', true, /\.stories\.(ts|js)$/);
 function loadStories() {
-  twig.keys().forEach(filename => twig(filename));
-  editor.keys().forEach(filename => editor(filename));
+  stories.keys().forEach(filename => stories(filename));
 }
 
 // Helps make UI components more accessible.
