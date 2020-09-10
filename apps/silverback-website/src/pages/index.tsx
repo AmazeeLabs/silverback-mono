2627501958bd52bf223230636b988bfb469cd3f0
Marco Tisi <marco.tisi@amazee.com>
2020-09-10 15:21:41 +0100

    chore(silverback-website): remove unnecessary plugins and pages

--- apps/silverback-website/src/pages/index.tsx
+++ apps/silverback-website/src/pages/index.tsx
@@ -1,5 +1,5 @@
 import React from "react"
-import {Link, PageProps} from "gatsby"
+import {PageProps} from "gatsby"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
@@ -10,7 +10,6 @@ const IndexPage: React.FC<PageProps> = () => (
     <h1>Hi people</h1>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
-    <Link to="/page-2/">Go to page 2</Link> <br />
   </Layout>
 )
 
