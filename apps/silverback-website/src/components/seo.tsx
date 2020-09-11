d67f9fed4b866be8d2974623a2c81d3a05ec29e5
Marco Tisi <marco.tisi@amazee.com>
2020-09-11 10:07:52 +0100

    style(silverback-website): lint and prettify source files

--- apps/silverback-website/src/components/seo.tsx
+++ apps/silverback-website/src/components/seo.tsx
@@ -5,9 +5,9 @@
  * See: https://www.gatsbyjs.org/docs/use-static-query/
  */
 
+import { graphql, useStaticQuery } from 'gatsby';
 import React from 'react';
 import { Helmet } from 'react-helmet';
-import { useStaticQuery, graphql } from 'gatsby';
 
 type SEOProps = {
   title: string;
--- apps/silverback-website/src/pages/404.tsx
+++ apps/silverback-website/src/pages/404.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
 import { PageProps } from 'gatsby';
+import React from 'react';
 
 import Layout from '../components/layout';
 import SEO from '../components/seo';
