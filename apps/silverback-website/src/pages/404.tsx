377a2602485a6781c60e8fbb1da22d402e66df4d
Marco Tisi <marco.tisi@amazee.com>
2020-09-11 09:27:13 +0100

    style(silverback-website): prettify source files

--- apps/silverback-website/src/pages/404.tsx
+++ apps/silverback-website/src/pages/404.tsx
@@ -1,8 +1,8 @@
-import React from "react"
-import { PageProps } from "gatsby"
+import React from 'react';
+import { PageProps } from 'gatsby';
 
-import Layout from "../components/layout"
-import SEO from "../components/seo"
+import Layout from '../components/layout';
+import SEO from '../components/seo';
 
 const NotFoundPage: React.FC<PageProps> = () => (
   <Layout>
@@ -10,6 +10,6 @@ const NotFoundPage: React.FC<PageProps> = () => (
     <h1>NOT FOUND</h1>
     <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
   </Layout>
-)
+);
 
-export default NotFoundPage
+export default NotFoundPage;
