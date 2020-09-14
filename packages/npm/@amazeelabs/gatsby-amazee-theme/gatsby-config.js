0c511ec8374c692056ea797ad8750d24a4904bc8
Marco Tisi <marco.tisi@gmail.com>
2020-09-14 19:33:59 +0100

    feat(gatsby-amazee-theme): add postcss-import postcss-preset-env postcss-discard-comments

--- packages/npm/@amazeelabs/gatsby-amazee-theme/gatsby-config.js
+++ packages/npm/@amazeelabs/gatsby-amazee-theme/gatsby-config.js
@@ -4,8 +4,14 @@ module.exports = ({ postCssPlugins = [], cssLoaderOptions = {} }) => ({
       resolve: `gatsby-plugin-postcss`,
       options: {
         postCssPlugins: [
+          // Transform @import rules by inlining content.
+          require('postcss-import'),
+          // Automatic prefixing and browser compatibility.
+          require('postcss-preset-env')({ stage: 0 }),
           // Apply tailwind features.
           require('tailwindcss')(),
+          // Strip CSS comments.
+          require('postcss-discard-comments'),
           // Add vendor prefixes.
           require('autoprefixer'),
           ...postCssPlugins,
