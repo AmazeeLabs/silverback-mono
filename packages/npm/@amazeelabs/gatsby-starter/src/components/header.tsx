8ed5cb2f36e7ceccc8ad4d7c32950c24a4b9b8e8
Marco Tisi <marco.tisi@gmail.com>
2020-09-14 12:07:53 +0100

    feat(gatsby-starter): extract gatsby starter from silverback-website

---
+++ packages/npm/@amazeelabs/gatsby-starter/src/components/header.tsx
@@ -0,0 +1,16 @@
+import { Link } from 'gatsby';
+import React from 'react';
+
+type HeaderProps = {
+  siteTitle: string;
+};
+
+const Header: React.FC<HeaderProps> = ({ siteTitle }) => (
+  <header>
+    <h1>
+      <Link to="/">{siteTitle}</Link>
+    </h1>
+  </header>
+);
+
+export default Header;
