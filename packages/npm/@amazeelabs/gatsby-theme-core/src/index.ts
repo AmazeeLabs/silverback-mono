5ff642604e3f1fafc57c45dd1c6d50fca6b374c6
Marco Tisi <marco.tisi@gmail.com>
2020-09-16 10:12:35 +0100

    refactor(gatsby-theme-core): export everything to enable short imports

---
+++ packages/npm/@amazeelabs/gatsby-theme-core/src/index.ts
@@ -0,0 +1,11 @@
+// Components
+export * from './components/footer';
+export * from './components/header';
+export * from './components/seo';
+
+// Hooks
+export * from './hooks/use-site-metadata';
+
+// Layout
+export * from './layout/layout';
+export * from './layout/wrap-page-element';
