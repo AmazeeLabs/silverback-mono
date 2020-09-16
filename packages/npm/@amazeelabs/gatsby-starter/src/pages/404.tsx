5ec46ef2a7f253382ccecc161d14c46f7d8d3c6e
Marco Tisi <marco.tisi@gmail.com>
2020-09-16 10:13:45 +0100

    refactor(gatsby-starter): use components from core theme

--- packages/npm/@amazeelabs/gatsby-starter/src/pages/404.tsx
+++ packages/npm/@amazeelabs/gatsby-starter/src/pages/404.tsx
@@ -1,15 +1,13 @@
+import { SEO } from '@amazeelabs/gatsby-theme-core';
 import { PageProps } from 'gatsby';
 import React from 'react';
 
-import Layout from '../components/layout';
-import SEO from '../components/seo';
-
 const NotFoundPage: React.FC<PageProps> = () => (
-  <Layout>
+  <>
     <SEO title="404: Not found" />
     <h1>NOT FOUND</h1>
     <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-  </Layout>
+  </>
 );
 
 export default NotFoundPage;
