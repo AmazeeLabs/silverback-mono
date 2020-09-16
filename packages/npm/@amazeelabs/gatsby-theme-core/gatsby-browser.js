0f6202bd8b4d1bafb321f7ff98d807354ffc68da
Marco Tisi <marco.tisi@gmail.com>
2020-09-16 10:12:56 +0100

    feat(gatsby-theme-core): wrap page with layout

---
+++ packages/npm/@amazeelabs/gatsby-theme-core/gatsby-browser.js
@@ -0,0 +1,8 @@
+/**
+ * Implement Gatsby's Browser APIs in this file.
+ *
+ * See: https://www.gatsbyjs.org/docs/browser-apis/
+ */
+import { WrapPageElement } from './src/layout/wrap-page-element';
+
+export const wrapPageElement = WrapPageElement;
