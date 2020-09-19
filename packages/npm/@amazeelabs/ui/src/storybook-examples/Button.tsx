8411f9c2151dabb31b7ef26580f1848c0334ae1a
Marco Tisi <marco.tisi@gmail.com>
2020-09-19 11:18:13 +0100

    style(ui): fix formatting and linting issues

--- packages/npm/@amazeelabs/ui/src/storybook-examples/Button.tsx
+++ packages/npm/@amazeelabs/ui/src/storybook-examples/Button.tsx
@@ -1,5 +1,6 @@
-import React from 'react';
 import './button.css';
+
+import React from 'react';
 
 export interface ButtonProps {
   /**
@@ -34,11 +35,15 @@ export const Button: React.FC<ButtonProps> = ({
   label,
   ...props
 }) => {
-  const mode = primary ? 'storybook-button--primary' : 'storybook-button--secondary';
+  const mode = primary
+    ? 'storybook-button--primary'
+    : 'storybook-button--secondary';
   return (
     <button
       type="button"
-      className={['storybook-button', `storybook-button--${size}`, mode].join(' ')}
+      className={['storybook-button', `storybook-button--${size}`, mode].join(
+        ' ',
+      )}
       style={{ backgroundColor }}
       {...props}
     >
