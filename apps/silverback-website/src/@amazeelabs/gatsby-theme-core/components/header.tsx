52228b592a91dbca0a8aae58db8e7de2de175bf0
Marco Tisi <marco.tisi@gmail.com>
2020-09-21 10:55:05 +0100

    feat(silverback-website): shadow Header component

---
+++ apps/silverback-website/src/@amazeelabs/gatsby-theme-core/components/header.tsx
@@ -0,0 +1,132 @@
+import { useSiteMetadata } from '@amazeelabs/gatsby-theme-core';
+import { Link } from 'gatsby';
+import React from 'react';
+
+import amazeeLogo from '../../../assets/logo.svg';
+
+export const Header: React.FC = () => {
+  const site = useSiteMetadata();
+
+  return (
+    <div className="relative bg-white">
+      <div className="max-w-7xl mx-auto px-4 sm:px-6">
+        <div className="flex justify-between items-center border-b-2 border-gray-100 py-6 md:justify-start md:space-x-10">
+          <Link to="/" className="flex">
+            <img
+              className="h-8 w-auto sm:h-10"
+              src={amazeeLogo}
+              alt={site.title}
+            />
+          </Link>
+          <div className="-mr-2 -my-2 md:hidden">
+            <button
+              type="button"
+              className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 focus:text-gray-500 transition duration-150 ease-in-out"
+            >
+              {/*{/*<!-- Heroicon name: menu -->*/}
+              <svg
+                className="h-6 w-6"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16M4 12h16M4 18h16"
+                />
+              </svg>
+            </button>
+          </div>
+          <nav className="hidden md:flex space-x-10">
+            <a
+              href="#"
+              className="text-base leading-6 font-medium text-gray-500 hover:text-gray-900 focus:outline-none focus:text-gray-900 transition ease-in-out duration-150"
+            >
+              Section #1
+            </a>
+            <a
+              href="#"
+              className="text-base leading-6 font-medium text-gray-500 hover:text-gray-900 focus:outline-none focus:text-gray-900 transition ease-in-out duration-150"
+            >
+              Section #2
+            </a>
+          </nav>
+        </div>
+      </div>
+
+      {/*
+        TODO: Add mobile menu
+        Mobile menu, show/hide based on mobile menu state.
+
+        Entering: "duration-200 ease-out"
+          From: "opacity-0 scale-95"
+          To: "opacity-100 scale-100"
+        Leaving: "duration-100 ease-in"
+          From: "opacity-100 scale-100"
+          To: "opacity-0 scale-95"
+      */}
+      <div className="hidden absolute top-0 inset-x-0 p-2 transition transform origin-top-right md:hidden">
+        <div className="rounded-lg shadow-lg">
+          <div className="rounded-lg shadow-xs bg-white divide-y-2 divide-gray-50">
+            <div className="pt-5 pb-6 px-5 space-y-6">
+              <div className="flex items-center justify-between">
+                <div>
+                  <Link to="/" className="flex">
+                    <img
+                      className="h-8 w-auto sm:h-10"
+                      src={amazeeLogo}
+                      alt={site.title}
+                    />
+                  </Link>
+                </div>
+                <div className="-mr-2">
+                  <button
+                    type="button"
+                    className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 focus:text-gray-500 transition duration-150 ease-in-out"
+                  >
+                    {/*<!-- Heroicon name: x -->*/}
+                    <svg
+                      className="h-6 w-6"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth="2"
+                        d="M6 18L18 6M6 6l12 12"
+                      />
+                    </svg>
+                  </button>
+                </div>
+              </div>
+              <div>
+                <nav className="grid gap-y-8">
+                  <a
+                    href="#"
+                    className="-m-3 p-3 flex items-center space-x-3 rounded-md hover:bg-gray-50 transition ease-in-out duration-150"
+                  >
+                    <div className="text-base leading-6 font-medium text-gray-900">
+                      Section #1
+                    </div>
+                  </a>
+                  <a
+                    href="#"
+                    className="-m-3 p-3 flex items-center space-x-3 rounded-md hover:bg-gray-50 transition ease-in-out duration-150"
+                  >
+                   <div className="text-base leading-6 font-medium text-gray-900">
+                      Section #2
+                    </div>
+                  </a>
+                </nav>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
