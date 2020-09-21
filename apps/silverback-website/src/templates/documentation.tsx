51efa48fecae27a9af3f809a6030ff3cee2df883
Marco Tisi <marco.tisi@gmail.com>
2020-09-21 12:27:53 +0100

    feat(silverback-website): improve styling of html elements

--- apps/silverback-website/src/templates/documentation.tsx
+++ apps/silverback-website/src/templates/documentation.tsx
@@ -1,6 +1,6 @@
 import { SEO } from '@amazeelabs/gatsby-theme-core';
 import { MDXProvider } from '@mdx-js/react';
-import { graphql, Link, PageProps } from 'gatsby';
+import { graphql, PageProps } from 'gatsby';
 import { MDXRenderer } from 'gatsby-plugin-mdx';
 import React from 'react';
 
@@ -16,7 +16,19 @@ export const pageQuery = graphql`
   }
 `;
 
-const shortcodes = { Link }; // Provide common components here
+const tagWithClassName = (
+  Tag: string,
+  className: string,
+): React.FC<{
+  Tag: JSX.Element;
+  className: string;
+  // eslint-disable-next-line react/display-name
+}> => ({ children, ...props }) => (
+  // @ts-ignore
+  <Tag {...props} className={className}>
+    {children}
+  </Tag>
+);
 
 const Documentation: React.FC<PageProps<{
   mdx: {
@@ -28,7 +40,51 @@ const Documentation: React.FC<PageProps<{
 }>> = ({ data: { mdx } }) => (
   <>
     <SEO title={mdx.frontmatter.title} />
-    <MDXProvider components={shortcodes}>
+    <MDXProvider
+      components={{
+        h1: tagWithClassName(
+          'h1',
+          'mb-4 -ml-1 text-3xl font-extrabold tracking-wider',
+        ),
+        h2: tagWithClassName(
+          'h2',
+          'mb-4 -ml-1 text-xl font-bold tracking-wide',
+        ),
+        h3: tagWithClassName(
+          'h3',
+          'mb-4 -ml-1 text-lg font-bold',
+        ),
+        h4: tagWithClassName(
+          'h4',
+          'mb-4 -ml-1 text-lg font-semibold',
+        ),
+        h5: tagWithClassName(
+          'h5',
+          'mb-4 -ml-1 text-lg font-medium',
+        ),
+        h6: tagWithClassName(
+          'h6',
+          'mb-4 -ml-1 text-base font-semibold',
+        ),
+        p: tagWithClassName(
+          'p',
+          'leading-relaxed font-light mt-2 mb-4',
+        ),
+        a: tagWithClassName(
+          'a',
+          'text-blue-700 hover:text-blue-500 leading-relaxed font-medium mb-8',
+        ),
+        blockquote: tagWithClassName(
+          'blockquote',
+          'border-solid border-l-4 border-gray-300 pl-4',
+        ),
+        ul: tagWithClassName('ul', 'list-disc m-4'),
+        ol: tagWithClassName('ol', 'list-decimal m-4'),
+        li: tagWithClassName('li', 'font-light my-1'),
+        strong: tagWithClassName('strong', 'font-semibold'),
+        em: tagWithClassName('em', 'italic'),
+      }}
+    >
       <MDXRenderer>{mdx.body}</MDXRenderer>
     </MDXProvider>
   </>
