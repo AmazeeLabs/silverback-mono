57b06d798efeb08c1b73f5c6c1c594b11b72f585
Philipp Melab <philipp.melab@gmail.com>
2020-09-22 08:56:50 +0200

    refactor(drupal/cypress): simplify step definitions, auto-uninstall drupal

--- packages/composer/drupal/cypress/tests/Cypress/integration/Session/index.js
+++ packages/composer/drupal/cypress/tests/Cypress/integration/Session/index.js
@@ -1,6 +1,35 @@
 // @ts-check
 /// <reference types="Cypress" />
-import {langCodes} from "../steps";
+
+const langCodes = {
+  'German': 'de'
+};
+
+beforeEach(() => {
+  cy.drupalInstall({
+    setup: 'cypress:integration/CypressTestSiteInstallScript.php',
+  });
+});
+
+// Given a test case uses 'cy.drupalSession' to authenticate in as "admin"
+Given(/^the test case uses 'cy.drupalSession' to authenticate as "([^"]*)"$/, function (account) {
+  cy.drupalSession({user: account});
+});
+
+// And the test case uses 'cy.drupalSession' to display switch to "German"
+And(/^the test case uses 'cy.drupalSession' to display switch to "([^"]*)"$/, function (language) {
+  cy.drupalSession({language: langCodes[language]});
+});
+
+// And the test case uses 'cy.drupalSession' to switch to workspace "stage"
+And(/^the test case uses 'cy.drupalSession' to switch to workspace "([^"]*)"$/, function (workspace) {
+  cy.drupalSession({workspace: workspace});
+});
+
+// And the test case uses 'cy.drupalSession' to display the toolbar
+Given(/^the test case uses 'cy.drupalSession' to display the toolbar$/, function () {
+  cy.drupalSession({toolbar: true});
+});
 
 // When the test case visits the homepage and clicks the link to the "admin" account
 When(/^the test case visits the homepage and clicks the link to the "([^"]*)" account$/, function (account) {
