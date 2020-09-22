cbb84e86e8991bbd01cca2ee9ea9447955071908
Marco Tisi <marco.tisi@gmail.com>
2020-09-22 19:08:10 +0100

    feat(silverback-website): style 404 page

--- apps/silverback-website/src/pages/404.tsx
+++ apps/silverback-website/src/pages/404.tsx
@@ -3,11 +3,11 @@ import { PageProps } from 'gatsby';
 import React from 'react';
 
 const NotFoundPage: React.FC<PageProps> = () => (
-  <>
+  <div className="bg-white rounded-lg shadow-xl px-5 py-6 sm:px-6">
     <SEO title="404: Not found" />
     <h1>NOT FOUND</h1>
     <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-  </>
+  </div>
 );
 
 export default NotFoundPage;
