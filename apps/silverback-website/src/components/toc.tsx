672ea75dfb773a8d6be394c4df0844570ded2cdb
Marco Tisi <marco.tisi@gmail.com>
2020-09-24 20:42:06 +0100

    fix(silverback-website): fix mobile TOC

--- apps/silverback-website/src/components/toc.tsx
+++ apps/silverback-website/src/components/toc.tsx
@@ -5,7 +5,7 @@ import { slugify } from '../utils';
 export const TOC: React.FC<{
   items: string[];
 }> = ({ items }) => (
-  <aside className="flex-none w-1/4 px-5 py-6 mb-8 bg-white rounded-lg shadow-xl sm:px-6 md:mr-4 md:sticky md:top-4 prose prose-sm sm:prose max-w-none sm:max-w-none">
+  <aside className="flex-none px-5 py-6 mb-8 bg-white rounded-lg shadow-xl md:w-1/4 sm:px-6 md:mr-4 md:sticky md:top-4 prose prose-sm sm:prose max-w-none sm:max-w-none">
     <h3 className="mt-0 font-bold">Table of contents</h3>
     <ul className="mb-0 list-none">
       {items.map((item, index) => (
