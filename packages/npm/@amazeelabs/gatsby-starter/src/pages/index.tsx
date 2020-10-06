a31c6244a5adc6cbdb8601be2371f3db6fced1ab
Philipp Melab <philipp.melab@gmail.com>
2020-10-06 14:28:50 +0200

    refactor: added example query typing to gatsby starter

--- packages/npm/@amazeelabs/gatsby-starter/src/pages/index.tsx
+++ packages/npm/@amazeelabs/gatsby-starter/src/pages/index.tsx
@@ -1,10 +1,20 @@
 import { SEO } from '@amazeelabs/gatsby-theme-core';
-import { PageProps } from 'gatsby';
+import { graphql, PageProps } from 'gatsby';
 import React from 'react';
 
-const IndexPage: React.FC<PageProps> = () => (
+export const query = graphql`
+  query HomePage {
+    site {
+      siteMetadata {
+        title
+      }
+    }
+  }
+`;
+
+const IndexPage: React.FC<PageProps & HomePageQuery> = ({ site }) => (
   <>
-    <SEO title="Home" />
+    <SEO title={site?.siteMetadata?.title || 'No title'} />
     <h1>Hi people</h1>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
