d684d2c46c56c71b81f1337368d4252337763bb0
Philipp Melab <philipp.melab@gmail.com>
2020-10-13 16:56:05 +0200

    fix: remove dependencies on deleted theme component

--- packages/npm/@amazeelabs/gatsby-starter/src/pages/404.tsx
+++ packages/npm/@amazeelabs/gatsby-starter/src/pages/404.tsx
@@ -1,10 +1,8 @@
-import { SEO } from '@amazeelabs/gatsby-theme-core';
 import { PageProps } from 'gatsby';
 import React from 'react';
 
 const NotFoundPage: React.FC<PageProps> = () => (
   <>
-    <SEO title="404: Not found" />
     <h1>NOT FOUND</h1>
     <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
   </>
