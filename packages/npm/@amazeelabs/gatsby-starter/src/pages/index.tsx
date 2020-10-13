fae8c05f2895c1b5ba1341f1675e9192dc3c0721
Philipp Melab <philipp.melab@gmail.com>
2020-10-13 16:56:05 +0200

    fix: remove dependencies on deleted theme component

--- packages/npm/@amazeelabs/gatsby-starter/src/pages/index.tsx
+++ packages/npm/@amazeelabs/gatsby-starter/src/pages/index.tsx
@@ -1,20 +1,8 @@
-import { SEO } from '@amazeelabs/gatsby-theme-core';
-import { graphql, PageProps } from 'gatsby';
+import { PageProps } from 'gatsby';
 import React from 'react';
 
-export const query = graphql`
-  query HomePage {
-    site {
-      siteMetadata {
-        title
-      }
-    }
-  }
-`;
-
-const IndexPage: React.FC<PageProps & HomePageQuery> = ({ site }) => (
+const IndexPage: React.FC<PageProps> = () => (
   <>
-    <SEO title={site?.siteMetadata?.title || 'No title'} />
     <h1>Hi people</h1>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
