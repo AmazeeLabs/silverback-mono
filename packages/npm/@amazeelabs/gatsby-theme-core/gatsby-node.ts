0367091d84b2428b89f3f578538e74c06183ddf7
Philipp Melab <philipp.melab@gmail.com>
2020-11-17 11:17:30 +0100

    refactor(gatsby-theme-core): proper typescript project configuration for gatsby-theme-core

---
+++ packages/npm/@amazeelabs/gatsby-theme-core/gatsby-node.ts
@@ -0,0 +1,12 @@
+import { GatsbyNode } from 'gatsby';
+import TsconfigPathsPlugin from 'tsconfig-paths-webpack-plugin';
+
+export const onCreateWebpackConfig: GatsbyNode['onCreateWebpackConfig'] = ({
+  actions,
+}) => {
+  actions.setWebpackConfig({
+    resolve: {
+      plugins: [new TsconfigPathsPlugin()],
+    },
+  });
+};
--- packages/npm/@amazeelabs/gatsby-theme-core/gatsby-ssr.ts
+++ packages/npm/@amazeelabs/gatsby-theme-core/gatsby-ssr.ts
@@ -1,9 +1,12 @@
-export const onPreRenderHTML = ({ getHeadComponents }) => {
+import { PreRenderHTMLArgs } from 'gatsby';
+import { isValidElement } from 'react';
+
+export const onPreRenderHTML = ({ getHeadComponents }: PreRenderHTMLArgs) => {
   if (process.env.NODE_ENV !== 'production') return;
 
   getHeadComponents().forEach((el) => {
     // Styles should not be loaded inline but as real stylesheet files.
-    if (el.type === 'style' && el.props['data-href']) {
+    if (isValidElement(el) && el.type === 'style' && el.props['data-href']) {
       el.type = 'link';
       el.props['href'] = el.props['data-href'];
       el.props['rel'] = 'stylesheet';
