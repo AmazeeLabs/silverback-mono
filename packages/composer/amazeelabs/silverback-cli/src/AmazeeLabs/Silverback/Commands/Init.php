2dc869b82830289518e5a3f213fee4c306354ba6
Alexander Tkachev <alex.tkachev@amazee.com>
2020-11-17 20:57:13 +0300

    fix(#483): remove leftover code
    closes #483
    
    so nice to code directly from github
    
    next time i should try to do it from a phone

--- packages/composer/amazeelabs/silverback-cli/src/AmazeeLabs/Silverback/Commands/Init.php
+++ packages/composer/amazeelabs/silverback-cli/src/AmazeeLabs/Silverback/Commands/Init.php
@@ -142,11 +142,6 @@ class Init extends SilverbackCommand {
     $composerJson['extra']['merge-plugin']['replace'] = TRUE;
 
     file_put_contents($this->rootDirectory . '/composer.json', json_encode(array_filter($composerJson), JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES));
-
-    // Link the storybook dist directory to the library dist folder.
-    if (!file_exists('web/themes/custom/storybook/dist')) {
-      symlink('../../../../storybook/dist', 'web/themes/custom/storybook/dist');
-    }
   }
 
 }
