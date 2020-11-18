0c52372afe049239b865f564640de57938716af6
Philipp Melab <philipp.melab@gmail.com>
2020-11-18 09:30:51 +0100

    refactor(gatsby-starter): removed unused import

--- packages/npm/@amazeelabs/gatsby-starter/.storybook/main.js
+++ packages/npm/@amazeelabs/gatsby-starter/.storybook/main.js
@@ -1,6 +1,3 @@
-// TODO: Move into preset and shared tsconfig.
-const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
-
 module.exports = {
   stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: ['@storybook/addon-essentials', '@amazeelabs/gatsby-theme-core'],
