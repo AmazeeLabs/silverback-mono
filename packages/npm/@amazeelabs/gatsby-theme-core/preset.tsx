854f5467365876f39b8f7498e37fc927b9c848e0
Philipp Melab <philipp.melab@gmail.com>
2020-11-24 10:31:34 +0100

    fix(gatsby-theme-core): add gatsby webpack rules to storybook preset

--- packages/npm/@amazeelabs/gatsby-theme-core/preset.tsx
+++ packages/npm/@amazeelabs/gatsby-theme-core/preset.tsx
@@ -3,8 +3,9 @@ import TsconfigPathsPlugin from 'tsconfig-paths-webpack-plugin';
 export const webpackFinal = (config: {
   module: {
     rules: {
+      include: RegExp;
       test: RegExp;
-      use: string[];
+      use: any[];
     }[];
   };
   resolve: {
@@ -12,6 +13,28 @@ export const webpackFinal = (config: {
   };
 }) => {
   config.resolve.plugins = [new TsconfigPathsPlugin()];
+  // Transpile Gatsby module because Gatsby includes un-transpiled ES6 code.
+  // https://www.gatsbyjs.org/docs/visual-testing-with-storybook/
+  config.module.rules.push({
+    test: /\.js$/,
+    include: /node_modules\/gatsby/,
+    use: [
+      {
+        loader: require.resolve('babel-loader'),
+        options: {
+          presets: [
+            require.resolve('@babel/preset-react'),
+            require.resolve('@babel/preset-env'),
+          ],
+          plugins: [
+            require.resolve('@babel/plugin-proposal-class-properties'),
+            require.resolve('babel-plugin-remove-graphql-queries'),
+          ],
+        },
+      },
+    ],
+  });
+
   return config;
 };
 
