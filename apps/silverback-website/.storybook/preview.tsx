f0544fe648203aa97d31e8711864e20c7bedb9f6
Philipp Melab <philipp.melab@gmail.com>
2020-12-02 16:48:11 +0100

    feat(jest-preset): added storybook snapshot integration

--- apps/silverback-website/.storybook/preview.tsx
+++ apps/silverback-website/.storybook/preview.tsx
@@ -3,10 +3,13 @@ import { addDecorator } from '@storybook/react';
 import '../src/assets/tailwind.css';
 import { dataMocks } from '../src/components/mocks';
 import { DataDependencyProvider } from '../src/components/dependencies';
+import { decorators } from '@amazeelabs/gatsby-theme-core/decorators';
 
 export const parameters = {
   actions: { argTypesRegex: '^on[A-Z].*' },
 };
+
+decorators.forEach(addDecorator);
 
 addDecorator((fn, c) => (
   <DataDependencyProvider dependencies={dataMocks}>
--- packages/npm/@amazeelabs/gatsby-starter/.storybook/preview.js
+++
@@ -1,5 +0,0 @@
-import '../src/assets/tailwind.css';
-
-export const parameters = {
-  actions: { argTypesRegex: '^on[A-Z].*' },
-};
