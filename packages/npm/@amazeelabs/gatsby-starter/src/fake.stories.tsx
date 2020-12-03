d88a4e661bd0a4382ce78e68dea04d07dc48ca56
Philipp Melab <philipp.melab@gmail.com>
2020-12-03 08:39:27 +0100

    fix(gatsby-starter): added missing storybook dependencies and a fake story to pass tests

---
+++ packages/npm/@amazeelabs/gatsby-starter/src/fake.stories.tsx
@@ -0,0 +1,10 @@
+
+import { Meta, Story } from '@storybook/react/types-6-0';
+import React from 'react';
+
+export default {
+  title: 'Fake story'
+} as Meta;
+
+export const Fake : Story = () => <p>A fake story so there is at least one of them. Delete me!</p>
+
