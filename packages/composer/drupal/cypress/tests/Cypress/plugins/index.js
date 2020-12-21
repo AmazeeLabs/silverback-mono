a7150b58a3fbb0e26409bff671751ae46277f5c6
Alex Tkachev <alex.leksat@gmail.com>
2020-12-21 14:58:58 +0300

    fix: let browserify know node_modules location
    details: https://github.com/cypress-io/code-coverage/issues/299#issuecomment-748933046

--- packages/composer/drupal/cypress/tests/Cypress/plugins/index.js
+++ packages/composer/drupal/cypress/tests/Cypress/plugins/index.js
@@ -1,5 +1,18 @@
+const path = require('path');
 const cucumber = require('cypress-cucumber-preprocessor').default;
+const browserify = require('@cypress/browserify-preprocessor');
 
 module.exports = (on, config) => {
-  on('file:preprocessor', cucumber());
+  const options = {
+    ...browserify.defaultOptions,
+    browserifyOptions: {
+      ...browserify.defaultOptions.browserifyOptions,
+      paths: [
+        // In case if some of the integration tests are symlinked, let
+        // browserify know the node_modules location.
+        path.join(__dirname, '../../node_modules'),
+      ],
+    }
+  };
+  on('file:preprocessor', cucumber(options));
 };
