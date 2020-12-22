d53511e97743d0a401947b165058a75fb83ce5be
Alex Tkachev <alex.leksat@gmail.com>
2020-12-22 15:14:15 +0300

    fix: make it work after SA-CORE-2020-008
    should solve #545

--- packages/composer/drupal/cypress/src/Authentication/Provider/CypressAuthenticationProvider.php
+++ packages/composer/drupal/cypress/src/Authentication/Provider/CypressAuthenticationProvider.php
@@ -5,7 +5,8 @@ namespace Drupal\cypress\Authentication\Provider;
 use Drupal\Core\Authentication\AuthenticationProviderFilterInterface;
 use Drupal\Core\Authentication\AuthenticationProviderInterface;
 use Drupal\Core\Entity\EntityTypeManagerInterface;
-use Drupal\Core\Session\AccountInterface;
+use Drupal\Core\Session\AccountProxy;
+use Symfony\Component\EventDispatcher\EventDispatcherInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Session\SessionInterface;
 
@@ -24,18 +25,24 @@ class CypressAuthenticationProvider implements AuthenticationProviderInterface, 
   protected $session;
 
   /**
+   * @var EventDispatcherInterface
+   */
+  protected $eventDispatcher;
+
+  /**
    * LocaleWorkspaceNegotiator constructor.
    *
    * @param \Drupal\Core\Entity\EntityTypeManagerInterface $entity_type_manager
-   *
    * @param \Symfony\Component\HttpFoundation\Session\SessionInterface $session
+   * @param \Symfony\Component\EventDispatcher\EventDispatcherInterface $eventDispatcher
    *
    * @throws \Drupal\Component\Plugin\Exception\InvalidPluginDefinitionException
    * @throws \Drupal\Component\Plugin\Exception\PluginNotFoundException
    */
-  public function __construct(EntityTypeManagerInterface $entity_type_manager, SessionInterface $session) {
+  public function __construct(EntityTypeManagerInterface $entity_type_manager, SessionInterface $session, EventDispatcherInterface $eventDispatcher) {
     $this->session = $session;
     $this->userStorage = $entity_type_manager->getStorage('user');
+    $this->eventDispatcher = $eventDispatcher;
   }
 
   /**
@@ -58,12 +65,22 @@ class CypressAuthenticationProvider implements AuthenticationProviderInterface, 
    * {@inheritDoc}
    */
   public function authenticate(Request $request) {
-    $matches = $this->userStorage->loadByProperties([
-      'name' => $this->getUserFromRequest($request),
-    ]);
-    $account = array_pop($matches);
-    if ($account instanceof AccountInterface) {
-      return $account;
+    $username = $this->getUserFromRequest($request);
+    if ($username) {
+      // Do not use the entity query to prevent a tricky issue with workspaces.
+      // See https://github.com/AmazeeLabs/silverback-mono/issues/545
+      $uid = \Drupal::database()
+        ->select('users_field_data', 'u')
+        ->fields('u', ['uid'])
+        ->condition('u.name', $username)
+        ->execute()
+        ->fetchField();
+      if ($uid) {
+        // Same here. Avoid loading the user entity.
+        $accountProxy = new AccountProxy($this->eventDispatcher);
+        $accountProxy->setInitialAccountId($uid);
+        return $accountProxy;
+      }
     }
     return NULL;
   }
