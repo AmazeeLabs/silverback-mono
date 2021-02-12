f6474beda17dcf919f37e8f081ee39274359191c
Philipp Melab <philipp.melab@gmail.com>
2021-02-12 07:56:44 +0100

    feat(drupal/cypress): enable testing callbacks via environment variable

--- packages/composer/drupal/cypress/tests/Cypress/interactions/index.js
+++ packages/composer/drupal/cypress/tests/Cypress/interactions/index.js
@@ -1,28 +1,28 @@
 import { createCypressQuestion } from 'cypress-screenplay';
 
-export const readCurrentUserName = createCypressQuestion((cy, _, assert) => {
-  cy.visit('/user');
-  cy.get('h1').should($items => {
+export const readCurrentUserName = createCypressQuestion((browser, _, assert) => {
+  browser.cy.visit('/user');
+  browser.cy.get('h1').should($items => {
     $items.toArray().forEach((item) => {
       assert(item.textContent)
     })
   });
 });
 
-export const isToolbarVisible = createCypressQuestion((cy, _, assert) => {
-  cy.visit('/');
-  cy.get('#toolbar-bar').should($items => {
+export const isToolbarVisible = createCypressQuestion((browser, _, assert) => {
+  browser.cy.visit('/');
+  browser.cy.get('#toolbar-bar').should($items => {
     assert($items.length === 1);
   });
 });
 
-export const readCurrentWorkspace = createCypressQuestion((cy, _, assert) => {
-  cy.visit('/');
-  cy.get('.toolbar-icon-workspace').should($items => $items.toArray().forEach(item => assert(item.textContent)));
+export const readCurrentWorkspace = createCypressQuestion((browser, _, assert) => {
+  browser.cy.visit('/');
+  browser.cy.get('.toolbar-icon-workspace').should($items => $items.toArray().forEach(item => assert(item.textContent)));
 });
 
-export const readCurrentLanguage = createCypressQuestion((cy, _, assert) => {
-  cy.visit('/').then(() => {
-    cy.get('html').should($items => assert($items.attr('lang')));
+export const readCurrentLanguage = createCypressQuestion((browser, _, assert) => {
+  browser.cy.visit('/').then(() => {
+    browser.cy.get('html').should($items => assert($items.attr('lang')));
   });
 });
