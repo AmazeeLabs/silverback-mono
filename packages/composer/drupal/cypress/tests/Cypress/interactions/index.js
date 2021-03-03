3ff00b20ddcf8941363f44f6a7cb7a710f4ecb55
Philipp Melab <philipp.melab@gmail.com>
2021-03-03 17:44:23 +0100

    fix(cypress): adjust tests to align with changes in cypress-screenplay

--- packages/composer/drupal/cypress/tests/Cypress/interactions/index.js
+++ packages/composer/drupal/cypress/tests/Cypress/interactions/index.js
@@ -1,8 +1,8 @@
 import { createCypressQuestion } from 'cypress-screenplay';
 
 export const readCurrentUserName = createCypressQuestion((browser, _, assert) => {
-  browser.cy.visit('/user');
-  browser.cy.get('h1').should($items => {
+  browser.visit('/user');
+  browser.get('h1').should($items => {
     $items.toArray().forEach((item) => {
       assert(item.textContent)
     })
@@ -10,19 +10,19 @@ export const readCurrentUserName = createCypressQuestion((browser, _, assert) =>
 });
 
 export const isToolbarVisible = createCypressQuestion((browser, _, assert) => {
-  browser.cy.visit('/');
-  browser.cy.get('#toolbar-bar').should($items => {
+  browser.visit('/');
+  browser.get('#toolbar-bar').should($items => {
     assert($items.length === 1);
   });
 });
 
 export const readCurrentWorkspace = createCypressQuestion((browser, _, assert) => {
-  browser.cy.visit('/');
-  browser.cy.get('.toolbar-icon-workspace').should($items => $items.toArray().forEach(item => assert(item.textContent)));
+  browser.visit('/');
+  browser.get('.toolbar-icon-workspace').should($items => $items.toArray().forEach(item => assert(item.textContent)));
 });
 
 export const readCurrentLanguage = createCypressQuestion((browser, _, assert) => {
-  browser.cy.visit('/').then(() => {
-    browser.cy.get('html').should($items => assert($items.attr('lang')));
+  browser.visit('/').then(() => {
+    browser.get('html').should($items => assert($items.attr('lang')));
   });
 });
