445cadd03acf3e5b88f1c7a934b5f652e40908d5
Philipp Melab <philipp.melab@gmail.com>
2021-03-08 09:08:04 +0100

    fix(jest-preset): conditionally enable jest features

--- packages/npm/@amazeelabs/jest-preset/jest-preset.js
+++ packages/npm/@amazeelabs/jest-preset/jest-preset.js
@@ -10,28 +10,28 @@ if (fs.existsSync('./tsconfig.json')) {
   }
 }
 
-module.exports = {
-  watchPlugins: [
-    'jest-watch-select-projects',
-    'jest-watch-typeahead/filename',
-    'jest-watch-typeahead/testname',
-  ],
-  projects: [
-    {
-      displayName: 'unit',
-      preset: 'ts-jest',
-      testEnvironment: 'node',
-      testMatch: [ "**/__tests__/**/*.ts", "**/?(*.)+(spec|test).ts" ],
-      moduleNameMapper,
-    },
-    {
-      displayName: 'dom',
-      preset: 'ts-jest',
-      testEnvironment: 'jsdom',
-      testMatch: [ "**/__tests__/**/*.tsx", "**/?(*.)+(spec|test).tsx" ],
-      moduleNameMapper,
-    },
-    {
+const projects = [
+  {
+    displayName: 'unit',
+    preset: 'ts-jest',
+    testEnvironment: 'node',
+    testMatch: [ "**/__tests__/**/*.ts", "**/?(*.)+(spec|test).ts" ],
+    moduleNameMapper,
+  },
+  {
+    displayName: 'dom',
+    preset: 'ts-jest',
+    testEnvironment: 'jsdom',
+    testMatch: [ "**/__tests__/**/*.tsx", "**/?(*.)+(spec|test).tsx" ],
+    moduleNameMapper,
+  },
+];
+
+if (fs.existsSync('./package.json')) {
+  const packageJson = JSON.parse(fs.readFileSync('./package.json'));
+  const dependencies = Object.keys(Object.assign(packageJson.dependencies || {}, packageJson.devDependencies || {}));
+  if(dependencies.includes('@storybook/addon-storyshots')) {
+    projects.push({
       displayName: 'storybook',
       preset: 'ts-jest',
       testEnvironment: 'jsdom',
@@ -40,11 +40,23 @@ module.exports = {
       transform: {
         '^.+\\.stories\\.jsx?$': '@storybook/addon-storyshots/injectFileName',
       },
-    },
-    {
-      displayName: 'lint',
-      runner: 'jest-runner-eslint',
-      testMatch: ['<rootDir>/**/*.{ts,tsx,js,jsx}'],
-    },
+    });
+  }
+}
+
+if (fs.existsSync('./.eslintrc.js')) {
+  projects.push({
+    displayName: 'lint',
+    runner: 'jest-runner-eslint',
+    testMatch: ['<rootDir>/**/*.{ts,tsx,js,jsx}'],
+  })
+}
+
+module.exports = {
+  watchPlugins: [
+    'jest-watch-select-projects',
+    'jest-watch-typeahead/filename',
+    'jest-watch-typeahead/testname',
   ],
+  projects,
 };
