e172046fc684fb1545a8209ff27c8aec8daa6b4f
Philipp Melab <philipp.melab@gmail.com>
2021-03-08 12:20:26 +0100

    fix(silverback-gatsby): increase test delays

--- apps/silverback-gatsby/cypress/integration/constants.ts
+++ apps/silverback-gatsby/cypress/integration/constants.ts
@@ -3,5 +3,5 @@ export const drupalNodeOpUrl = 'http://localhost:8888/node-op';
 export const previewUrl = 'http://localhost:8000';
 export const siteUrl = 'http://localhost:9000';
 
-export const refreshDelay = 10_000;
-export const rebuildDelay = 20_000;
+export const refreshDelay = 15_000;
+export const rebuildDelay = 30_000;
