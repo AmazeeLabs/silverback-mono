52ac492b81eac1439e6096f192de703a12fc5398
Alex Tkachev <alex.leksat@gmail.com>
2021-03-08 13:56:45 +0300

    fix: passwords in exported uses

--- apps/silverback-drupal/web/modules/custom/silverback_gatsby_test/export_content.sh
+++ apps/silverback-drupal/web/modules/custom/silverback_gatsby_test/export_content.sh
@@ -17,3 +17,10 @@ drush dcer user --folder="$DIR"
 
 # Only leave the GatsbyPreview user.
 grep -L GatsbyPreview "$DIR/user/*.*" | xargs rm
+
+# Fix passwords.
+# https://www.drupal.org/project/default_content/issues/2943458#comment-14022041
+for file in $(find "$DIR/user" -type f)
+do
+    sed 's/pre_hashed: false/pre_hashed: true/g' "$file" > temp.txt && mv temp.txt "$file"
+done
