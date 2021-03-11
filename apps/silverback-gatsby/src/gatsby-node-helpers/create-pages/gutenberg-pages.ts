518eeacca7a1563d0ed310ea167292d8b577b021
Alex Tkachev <alex.leksat@gmail.com>
2021-03-11 16:04:43 +0300

    chore: add gutenberg pages to gatsby
    no parsing yet

---
+++ apps/silverback-gatsby/src/gatsby-node-helpers/create-pages/gutenberg-pages.ts
@@ -0,0 +1,53 @@
+import { GatsbyNode } from 'gatsby';
+
+import { languages } from '../../constants/languages';
+import { GutenbergPageContext } from '../../types/page-context';
+
+export const createGutenbergPages: Required<GatsbyNode>['createPages'] = async ({
+  graphql,
+  actions,
+}) => {
+  const { data, errors } = await graphql<AllGutenbergPagesQuery>(`
+    query AllGutenbergPages {
+      allDrupalGutenbergPage {
+        nodes {
+          id
+          translations {
+            langcode
+            path
+            title
+            body
+          }
+        }
+      }
+    }
+  `);
+  if (!data) {
+    console.error('errors', errors);
+    throw new Error('Cannot fetch Gutenberg pages from Gatsby.');
+  }
+  data.allDrupalGutenbergPage.nodes.forEach((page) =>
+    page.translations.forEach((translation) => {
+      const context: GutenbergPageContext = {
+        page: translation,
+        otherLanguages: page.translations
+          .filter((it) => it.langcode !== translation.langcode)
+          .map((other) => ({
+            path: other.path,
+            language: languages.find((it) => it.id === other.langcode)!,
+          })),
+      };
+
+      const path = translation.path;
+      const component = require.resolve(`../../components/gutenberg-page`);
+
+      // TODO: remove once the stale page data issue is fixed.
+      //  https://github.com/gatsbyjs/gatsby/issues/26520
+      // Temporary fix: Call createPage twice with different contexts. This
+      // helps Gatsby to refresh the page data.
+      actions.createPage({ path, component, context: 'fake context' });
+
+      return actions.createPage({ path, component, context });
+    }),
+  );
+};
