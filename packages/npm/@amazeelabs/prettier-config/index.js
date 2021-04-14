88b386caba67022d4db38c447aad8cbf84f85de7
Philipp Melab <philipp.melab@gmail.com>
2021-04-14 13:36:36 +0200

    feat(tooling): add and configure prettier for php

--- packages/npm/@amazeelabs/prettier-config/index.js
+++ packages/npm/@amazeelabs/prettier-config/index.js
@@ -1,6 +1,10 @@
 module.exports = {
   singleQuote: true,
   trailingComma: "all",
-  proseWrap: "always"
+  proseWrap: "always",
+  phpVersion: "7.4",
+  trailingCommaPHP: true,
+  braceStyle: "1tbs",
+  tabWidth: 2
 };
 
