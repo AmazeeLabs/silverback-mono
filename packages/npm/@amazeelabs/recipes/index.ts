b5472715527722ddf1a8ab98a57332bd111001ea
Philipp Melab <philipp.melab@gmail.com>
2021-04-20 07:51:01 +0200

    fix(recipes): use sync prompt to avoid hang on second one
    When the first prompt in index.ts is done asynchronously, the next one in the recipe would not return.

--- packages/npm/@amazeelabs/recipes/index.ts
+++ packages/npm/@amazeelabs/recipes/index.ts
@@ -1,6 +1,5 @@
 import fs from 'fs';
 import path from 'path';
-import prompts from 'prompts';
 import updateNotifier from 'update-notifier';
 
 import $ from './helpers';
@@ -13,31 +12,36 @@ const notifier = updateNotifier({ pkg });
 notifier.notify({ isGlobal: true });
 
 // Read all files from the recipes folder.
-const files = fs.readdirSync(path.resolve(__dirname, 'recipes'));
+const files = fs
+  .readdirSync(path.resolve(__dirname, 'recipes'))
+  .filter((file) => /\.js$/.test(file));
 const recipes = files.map((file) => path.parse(file).name);
 
 const arg = process.argv[2];
-(async () => {
-  // If the first CLI argument is a valid recipe, execute it.
-  // Else, prompt the user to choose one.
-  const recipe = recipes.includes(arg)
-    ? arg
-    : (
-        await prompts([
-          {
-            type: 'select',
-            name: 'Recipe',
-            message: 'Pick a recipe',
-            choices: recipes.map((recipe) => ({
-              title: recipe,
-              value: recipe,
-            })),
-          },
-        ])
-      ).Recipe;
-  try {
-    require(path.resolve(__dirname, 'recipes', recipe));
-  } catch (err) {
-    $.log.prettyError(err, true, true, true, 1);
-  }
-})();
+
+// If the first CLI argument is a valid recipe, execute it.
+// Else, prompt the user to choose one.
+const recipe = recipes.includes(arg)
+  ? arg
+  : $.prompts([
+      {
+        type: 'select',
+        name: 'Recipe',
+        message: 'Pick a recipe',
+        choices: recipes.map((recipe) => ({
+          title: recipe,
+          value: recipe,
+        })),
+      },
+    ]).Recipe;
+
+if (!recipe) {
+  $.log.warn('No recipe selected.');
+  process.exit(1);
+}
+
+try {
+  require(path.resolve(__dirname, 'recipes', recipe));
+} catch (err) {
+  $.log.prettyError(err, true, true, true, 1);
+}
