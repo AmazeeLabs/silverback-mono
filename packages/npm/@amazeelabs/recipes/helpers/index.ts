9fa2d5469dcef0f25628c21bc0a3904390449fb6
Philipp Melab <philipp.melab@gmail.com>
2021-04-20 08:07:08 +0200

    refactor(recipes): less verbose syntax for "run"

--- packages/npm/@amazeelabs/recipes/helpers/index.ts
+++ packages/npm/@amazeelabs/recipes/helpers/index.ts
@@ -1,13 +1,19 @@
 import * as files from './files';
 import * as logger from './logger';
-import * as process from './process';
+import { run } from './process';
 import * as prompts from './prompts';
 import * as versions from './versions';
 
-export default {
-  ...process,
+// "Run" is the most common command, and can be invoked directly with $('...')
+const $: typeof run &
+  typeof logger &
+  typeof files &
+  typeof prompts &
+  typeof versions = Object.assign(run, {
   ...logger,
   ...versions,
   ...prompts,
   ...files,
-};
+});
+
+export default $;
