92464b5d47f05aaba5af0bda5ed0979a43b328f4
Alex Tkachev <alex.leksat@gmail.com>
2021-06-23 12:19:45 +0300

    fix(gatsby): improve access checks in list_entities producer

--- packages/composer/amazeelabs/silverback_gatsby/src/Plugin/GraphQL/DataProducer/EntityQueryBase.php
+++ packages/composer/amazeelabs/silverback_gatsby/src/Plugin/GraphQL/DataProducer/EntityQueryBase.php
@@ -2,21 +2,37 @@
 
 namespace Drupal\silverback_gatsby\Plugin\GraphQL\DataProducer;
 
+use Drupal\Core\Cache\RefinableCacheableDependencyInterface;
 use Drupal\Core\Entity\Query\QueryInterface;
 use Drupal\graphql\Plugin\GraphQL\DataProducer\DataProducerPluginBase;
 
 class EntityQueryBase extends DataProducerPluginBase {
 
-  protected function getQuery(string $type): QueryInterface {
+  protected function getQuery(string $type, RefinableCacheableDependencyInterface $metadata): QueryInterface {
+    $currentUser = \Drupal::currentUser();
+    $entityType = \Drupal::entityTypeManager()->getDefinition($type);
+
     $query = \Drupal::entityQuery($type)
       ->currentRevision()
       ->accessCheck();
 
     // Add access conditions. Because accessCheck() does nothing by default. For
     // example it does not filter out unpublished nodes for anonymous users.
-    if ($type === 'node' && !\Drupal::currentUser()->hasPermission('bypass node access')) {
+    // Special permission for nodes.
+    if ($type === 'node' && !$currentUser->hasPermission('bypass node access')) {
       $query->condition('status', 1);
     }
+    // Admin permission for other entity types.
+    if (
+      ($publishedKey = $entityType->getKey('published')) &&
+      ($adminPermission = $entityType->getAdminPermission()) &&
+      !$currentUser->hasPermission($adminPermission)
+    ) {
+      $query->condition($publishedKey, 1);
+    }
+
+    $metadata->addCacheTags($entityType->getListCacheTags());
+    $metadata->addCacheContexts($entityType->getListCacheContexts());
 
     return $query;
   }
