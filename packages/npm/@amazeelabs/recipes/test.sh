06062d49d20ca0cab2b5d708caa0a14fe622e776
Philipp Melab <philipp.melab@gmail.com>
2021-07-14 09:22:50 +0200

    fix(recipes): get storybook recipe operational and automatically test it

--- packages/npm/@amazeelabs/recipes/test.sh
+++ packages/npm/@amazeelabs/recipes/test.sh
@@ -2,13 +2,12 @@
 
 set -e
 
-export LOG="silly"
-
 yarn config set script-shell /bin/bash
 
 rm -rf test
 echo "test" | node ./dist/index.js create-monorepo
 cd test || exit 1
+
 git add README.md
 git commit -m "chore: executed 'create-monorepo' recipe"
 node ../dist/index.js add-gatsby
@@ -17,6 +16,9 @@ git commit -m "chore: executed 'add-gatsby' recipe"
 node ../dist/index.js add-drupal
 git add README.md
 git commit -m "chore: executed 'add-drupal' recipe"
+node ../dist/index.js add-storybook
+git add README.md
+git commit -m "chore: executed 'add-storybook' recipe"
 
 
 cd .. || exit
