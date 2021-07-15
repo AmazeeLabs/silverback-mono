e3faf5a4a6ff725291ec219600659fd44f8465b8
Alex Tkachev <alex.leksat@gmail.com>
2021-07-15 14:41:18 +0300

    test: init drupal before running tests

--- packages/composer/amazeelabs/silverback_gutenberg/test.sh
+++ packages/composer/amazeelabs/silverback_gutenberg/test.sh
@@ -2,6 +2,6 @@
 set -e
 cd ../../../../apps/silverback-drupal || exit 1
 source .envrc
-#composer install
-#vendor/bin/silverback setup
+composer install
+vendor/bin/silverback setup
 vendor/phpunit/phpunit/phpunit web/modules/contrib/silverback_gutenberg
