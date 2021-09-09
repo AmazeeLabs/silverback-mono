c8485a84b0b5611ce702cf27a3203c2bf4b5a863
Alex Tkachev <alex.leksat@gmail.com>
2021-09-09 15:53:20 +0300

    feat: record traces on first retry

--- packages/npm/@amazeelabs/silverback-playwright/src/base.playwright.config.ts
+++ packages/npm/@amazeelabs/silverback-playwright/src/base.playwright.config.ts
@@ -3,7 +3,9 @@ import { PlaywrightTestConfig } from '@playwright/test';
 const config: PlaywrightTestConfig = {
   use: {
     screenshot: 'only-on-failure',
+    trace: 'on-first-retry',
   },
+  retries: 1,
   testDir: process.env.SP_TEST_DIR,
 };
 export default config;
