81bf63d299b830554342f89bff5ad753205c7658
Alex Tkachev <alex.leksat@gmail.com>
2021-09-14 16:57:04 +0300

    chore: increase global timeout

--- packages/npm/@amazeelabs/silverback-playwright/src/base.playwright.config.ts
+++ packages/npm/@amazeelabs/silverback-playwright/src/base.playwright.config.ts
@@ -7,5 +7,8 @@ const config: PlaywrightTestConfig = {
   },
   retries: 1,
   testDir: process.env.SP_TEST_DIR,
+  // Since a single test may waitForGatsby multiple times, the default 30s
+  // timeout is likely to be exceeded in the gatsby-build mode.
+  timeout: 60_000,
 };
 export default config;
