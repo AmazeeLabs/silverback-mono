3446b92424199bb2490ee464dd32c308b14ca97c
Alex Tkachev <alex.leksat@gmail.com>
2021-09-14 16:55:28 +0300

    fix: kill all gatsby ports to avoid hanging processes
    E.g. to avoid `gatsby develop` process still handing during gatsby-build tests.

--- packages/npm/@amazeelabs/silverback-playwright/src/utils.ts
+++ packages/npm/@amazeelabs/silverback-playwright/src/utils.ts
@@ -3,9 +3,16 @@ import { check, waitUntilUsed } from 'tcp-port-used';
 import { sleep } from 'zx';
 
 export const port = {
-  killIfUsed: async (port: number) => {
-    if (await check(port)) {
-      await kill(port);
+  killIfUsed: async (port: number | Array<number>) => {
+    const ports = typeof port === 'number' ? [port] : port;
+    let wait = false;
+    for (const current of ports) {
+      if (await check(current)) {
+        await kill(current);
+        wait = true;
+      }
+    }
+    if (wait) {
       // Somehow the port isn't yet available if we try to use it immediately.
       await sleep(1000);
     }
