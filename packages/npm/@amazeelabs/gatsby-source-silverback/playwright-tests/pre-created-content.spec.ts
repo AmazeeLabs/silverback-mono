9218c9f05ad1f087b4e48f7b7fc1b84fff0a2182
Alex Tkachev <alex.leksat@gmail.com>
2021-09-14 17:08:10 +0300

    refactor: rewrite tests with playwright and move them to packages

---
+++ packages/npm/@amazeelabs/gatsby-source-silverback/playwright-tests/pre-created-content.spec.ts
@@ -0,0 +1,47 @@
+import { gatsby, resetState } from '@amazeelabs/silverback-playwright';
+import { expect, test } from '@playwright/test';
+
+test.beforeAll(async () => {
+  await resetState();
+});
+
+test('@gatsby-develop not published article should be visible in the develop mode', async ({
+  page,
+}) => {
+  await page.goto(gatsby.baseUrl);
+  await expect(page.locator('a:text-is("Not published")')).toBeVisible();
+});
+
+test('@gatsby-build not published article should not be visible in the build mode', async ({
+  page,
+}) => {
+  await page.goto(gatsby.baseUrl);
+  await expect(page.locator('a:text-is("Not published")')).not.toBeVisible();
+});
+
+test('@gatsby-both other pre-created content', async ({ page }) => {
+  await page.goto(gatsby.baseUrl);
+
+  // Articles.
+  await expect(page.locator('a:text-is("With everything")')).toBeVisible();
+  await expect(page.locator('a:text-is("With everything DE")')).toBeVisible();
+  await expect(page.locator('a:text-is("With everything FR")')).toBeVisible();
+  await expect(page.locator('a:text-is("Article promoted")')).toBeVisible();
+
+  await page.click('a:text-is("With everything")');
+
+  // "With everything" article should have some images.
+  await expect(page.locator('img[alt="Kitten alt text"]')).toBeVisible();
+  await expect(page.locator('img[alt="Pug alt text"]')).toBeVisible();
+
+  // "With everything" article should be translated to all languages.
+  await expect(page.locator('a:text-is("German")')).toBeVisible();
+  await expect(page.locator('a:text-is("French")')).toBeVisible();
+
+  await page.goBack();
+
+  // "Gutenberg page" page should have no translations.
+  await page.click('a:text-is("Gutenberg page")');
+  await expect(page.locator('a:text-is("German")')).not.toBeVisible();
+  await expect(page.locator('a:text-is("French")')).not.toBeVisible();
+});
