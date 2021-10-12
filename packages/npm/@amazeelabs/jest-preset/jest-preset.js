05a1409d995a2e3f1e7028ff383d2bc189315570
Philipp Melab <philipp.melab@gmail.com>
2021-10-12 13:00:40 +0200

    refactor(scaffold): don't run eslint within jest

--- packages/npm/@amazeelabs/jest-preset/jest-preset.js
+++ packages/npm/@amazeelabs/jest-preset/jest-preset.js
@@ -1,13 +1,13 @@
-const fs = require('fs');
+const fs = require("fs");
 const moduleNameMapper = {
-  '\\.(css|less|scss|sss|styl)$': 'identity-obj-proxy',
+  "\\.(css|less|scss|sss|styl)$": "identity-obj-proxy",
 };
 
 const projects = [
   {
-    displayName: 'unit',
-    preset: 'ts-jest',
-    testEnvironment: 'node',
+    displayName: "unit",
+    preset: "ts-jest",
+    testEnvironment: "node",
     testMatch: [
       "**/__tests__/**/*.ts",
       "**/?(*.)+(spec|test).ts",
@@ -17,9 +17,9 @@ const projects = [
     moduleNameMapper,
   },
   {
-    displayName: 'dom',
-    preset: 'ts-jest',
-    testEnvironment: 'jsdom',
+    displayName: "dom",
+    preset: "ts-jest",
+    testEnvironment: "jsdom",
     testMatch: [
       "**/__tests__/**/*.tsx",
       "**/?(*.)+(spec|test).tsx",
@@ -30,36 +30,33 @@ const projects = [
   },
 ];
 
-if (fs.existsSync('./package.json')) {
-  const packageJson = JSON.parse(fs.readFileSync('./package.json'));
-  const dependencies = Object.keys(Object.assign(packageJson.dependencies || {}, packageJson.devDependencies || {}));
-  if(dependencies.includes('@storybook/addon-storyshots')) {
+if (fs.existsSync("./package.json")) {
+  const packageJson = JSON.parse(fs.readFileSync("./package.json"));
+  const dependencies = Object.keys(
+    Object.assign(
+      packageJson.dependencies || {},
+      packageJson.devDependencies || {}
+    )
+  );
+  if (dependencies.includes("@storybook/addon-storyshots")) {
     projects.push({
-      displayName: 'storybook',
-      preset: 'ts-jest',
-      testEnvironment: 'jsdom',
-      testMatch: [ "**/.storyshots.tsx" ],
+      displayName: "storybook",
+      preset: "ts-jest",
+      testEnvironment: "jsdom",
+      testMatch: ["**/.storyshots.tsx"],
       moduleNameMapper,
       transform: {
-        '^.+\\.stories\\.jsx?$': '@storybook/addon-storyshots/injectFileName',
+        "^.+\\.stories\\.jsx?$": "@storybook/addon-storyshots/injectFileName",
       },
     });
   }
 }
 
-if (fs.existsSync('./.eslintrc.js')) {
-  projects.push({
-    displayName: 'lint',
-    runner: 'jest-runner-eslint',
-    testMatch: ['<rootDir>/**/*.{ts,tsx,js,jsx}'],
-  })
-}
-
 module.exports = {
   watchPlugins: [
-    'jest-watch-select-projects',
-    'jest-watch-typeahead/filename',
-    'jest-watch-typeahead/testname',
+    "jest-watch-select-projects",
+    "jest-watch-typeahead/filename",
+    "jest-watch-typeahead/testname",
   ],
   projects,
 };
