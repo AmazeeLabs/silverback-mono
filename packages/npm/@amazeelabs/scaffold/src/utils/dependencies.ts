9c0a0daaff521f8744f7b2bf28b324ea6fcdcf4e
Philipp Melab <philipp.melab@gmail.com>
2021-10-14 11:13:40 +0200

    fix(scaffold): add typescript as a basic dependency

--- packages/npm/@amazeelabs/scaffold/src/utils/dependencies.ts
+++ packages/npm/@amazeelabs/scaffold/src/utils/dependencies.ts
@@ -3,12 +3,14 @@ import chalk from 'chalk';
 import eslintConfig from '../../../eslint-config/package.json';
 import jestPreset from '../../../jest-preset/package.json';
 import prettierConfig from '../../../prettier-config/package.json';
+import localPackages from '../../package.json';
 import { readPackageInfo, writePackageInfo } from './helpers';
 
 export function installDependencies(targetPath: string) {
   const targetInfo = readPackageInfo(targetPath);
 
   const dependenciesToInstall = {
+    typescript: `^${localPackages.devDependencies.typescript}`,
     [jestPreset.name]: `^${jestPreset.version}`,
     ...jestPreset.peerDependencies,
     [eslintConfig.name]: `^${eslintConfig.version}`,
