102605ecfe766330ba30973321f53969ed008f4f
Alex Tkachev <alex.leksat@gmail.com>
2021-11-23 15:17:55 +0300

    chore: increase test timeout
    Follow-up for cd3583cc3e30b3d89d1f214d60909aa526c15492

--- packages/npm/@amazeelabs/silverback-playwright/src/base.playwright.config.ts
+++ packages/npm/@amazeelabs/silverback-playwright/src/base.playwright.config.ts
@@ -8,6 +8,6 @@ const config: PlaywrightTestConfig = {
   testDir: process.env.SP_TEST_DIR,
   // Since a single test may waitForGatsby multiple times, the default 30s
   // timeout is likely to be exceeded in the gatsby-build mode.
-  timeout: 60_000,
+  timeout: 90_000,
 };
 export default config;
