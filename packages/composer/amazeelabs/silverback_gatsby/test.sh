4f4c6eb6d9b2817b74f2726624603a49dd23c2cc
Alex Tkachev <alex.leksat@gmail.com>
2021-12-23 14:30:02 +0300

    chore: help renovate to solve composer conflicts
    - update drupal/gutenberg (because it was one of the composer conflict reason)
    - remove drupal/cypress-dev and all the phpstan related things
      - because we still do not use phpstan
      - because phpstan was one of the composer conflict reason
    - remove useless dependencies

--- packages/composer/amazeelabs/silverback_gatsby/test.sh
+++ packages/composer/amazeelabs/silverback_gatsby/test.sh
@@ -4,5 +4,4 @@ cd ../../../../apps/silverback-drupal || exit 1
 source .envrc
 composer install
 vendor/bin/silverback setup
-#vendor/bin/phpstan analyse --configuration=web/modules/contrib/silverback_gatsby/phpstan.neon
 vendor/phpunit/phpunit/phpunit web/modules/contrib/silverback_gatsby
