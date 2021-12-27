9a855023850488190f9e8676dd5c42cef4a07217
Alex Tkachev <alex.leksat@gmail.com>
2021-12-27 10:16:09 +0300

    chore: try git add

--- packages/npm/@amazeelabs/sync-composer-version/sync-composer-version.js
+++ packages/npm/@amazeelabs/sync-composer-version/sync-composer-version.js
@@ -1,8 +1,11 @@
 #!/usr/bin/env node
 
 const { outputJSONSync, readJSONSync } = require("fs-extra");
+const { execSync } = require("child_process");
 
 const composer = readJSONSync("composer.json");
 const npm = readJSONSync("package.json");
 composer.version = npm.version;
 outputJSONSync("composer.json", composer, { spaces: 2 });
+
+execSync("git add .");
