b6b52d8e394e34caff70e757cf1af9c22cb0a84b
Alex Tkachev <alex.leksat@gmail.com>
2022-01-27 15:50:04 +0300

    fix: compatibility with gutenberg 2.4

--- packages/composer/amazeelabs/silverback_gutenberg/src/Service/MediaService.php
+++ packages/composer/amazeelabs/silverback_gutenberg/src/Service/MediaService.php
@@ -7,16 +7,18 @@ use Drupal\media_library\MediaLibraryState;
 
 class MediaService extends Original {
 
-  public function renderDialog(array $media_types) {
+  public function renderDialog(array $media_types, array $media_bundles = NULL) {
     // Instead of guessing media types as the parent method does, use given
     // media types as media type IDs. So the blocks have full control over
     // allowed media types.
-    // This behavior is proposed here:
-    // https://www.drupal.org/project/gutenberg/issues/3107837#comment-14119332
-    $allowed_media_type_ids = $media_types;
+    // The default behavior is: get bundles by $media_types, then intersect them
+    // with $media_bundles.
+    // New behavior is: get bundles from $media_types, so we don't break
+    // projects using this module.
+    $media_bundles = $media_types;
 
     $buildUi = $this->builder->buildUi(
-      MediaLibraryState::create('gutenberg.media_library.opener', array_unique($allowed_media_type_ids), reset($allowed_media_type_ids), 1)
+      MediaLibraryState::create('gutenberg.media_library.opener', array_unique($media_bundles), reset($media_bundles), 1)
     );
     $this->moduleHandler->alter('gutenberg_media_library_view', $buildUi);
     return $this->renderer->render($buildUi);
