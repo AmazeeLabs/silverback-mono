0370685e8f2ded938e99afd449dd5b421e661099
Luqmaan Essop <luqmaan.essop@amazee.com>
2022-01-31 10:50:36 +0200

    bring addon-a11y package

--- packages/npm/@silverback/ui/.storybook/main.js
+++ packages/npm/@silverback/ui/.storybook/main.js
@@ -2,7 +2,8 @@ module.exports = {
   "stories": ['../src/**/*.stories.@(js|jsx|ts|tsx|mdx)'],
   "addons": [
     "@storybook/addon-links",
-    "@storybook/addon-essentials"
+    "@storybook/addon-essentials",
+    "@storybook/addon-a11y"
   ],
   features: {
     previewCsfV3: true,
