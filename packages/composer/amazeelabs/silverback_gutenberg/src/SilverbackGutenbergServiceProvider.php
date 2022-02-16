03a2b44288cd613460fcc81787d0a6fd5c3a4fb8
Alex Tkachev <alex.leksat@gmail.com>
2022-02-16 13:05:43 +0300

    fix(gutenberg): remove the media bundles workaround

--- packages/composer/amazeelabs/silverback_gutenberg/src/SilverbackGutenbergServiceProvider.php
+++ packages/composer/amazeelabs/silverback_gutenberg/src/SilverbackGutenbergServiceProvider.php
@@ -5,14 +5,9 @@ namespace Drupal\silverback_gutenberg;
 use Drupal\Core\DependencyInjection\ContainerBuilder;
 use Drupal\Core\DependencyInjection\ServiceProviderBase;
 use Drupal\silverback_gutenberg\Normalizer\GutenbergContentEntityNormalizer;
-use Drupal\silverback_gutenberg\Service\MediaService;
 
 class SilverbackGutenbergServiceProvider extends ServiceProviderBase {
   public function alter(ContainerBuilder $container) {
-    if ($container->has('gutenberg.media_service')) {
-      $definition = $container->getDefinition('gutenberg.media_service');
-      $definition->setClass(MediaService::class);
-    }
     if ($container->has('webform.message_manager')) {
       $definition = $container->getDefinition('webform.message_manager');
       $definition->setClass(WebformMessageManager::class);
