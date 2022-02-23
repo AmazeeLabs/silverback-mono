21dd9410b91eb47f13c34cf30035997b7a00ba10
Philipp Melab <philipp.melab@gmail.com>
2022-02-23 14:57:27 +0100

    feat(publisher): on-screen build indicator for previews

--- packages/npm/@amazeelabs/publisher/src/refresh.ts
+++ packages/npm/@amazeelabs/publisher/src/refresh.ts
@@ -1,6 +1,9 @@
+import { html, LitElement } from 'lit';
+import { customElement, state } from 'lit/decorators.js';
 import { filter, of, scan } from 'rxjs';
 import { webSocket } from 'rxjs/webSocket';
 
+import style from './refresh.css';
 import type { StatusUpdate } from './server/logging';
 import { BuildState, GatewayState } from './states';
 
@@ -30,25 +33,61 @@ function createWebsocket() {
 
 export const updates$ = createWebsocket();
 
-updates$
-  .pipe(
-    scan(
-      (acc, value) => ({
-        previousBuilder: value.builder,
-        previousGateway: value.gateway,
-        refresh:
-          (value.builder !== acc.previousBuilder &&
-            value.builder === BuildState.Finished) ||
-          value.gateway !== acc.previousGateway,
-      }),
-      {
-        previousBuilder: BuildState.Finished,
-        previousGateway: GatewayState.Ready,
-        refresh: false,
-      },
-    ),
-    filter((item) => item.refresh),
-  )
-  .subscribe(() => {
-    window.location.reload();
-  });
+const refreshSignal$ = updates$.pipe(
+  scan(
+    (acc, value) => ({
+      previousBuilder: value.builder,
+      previousGateway: value.gateway,
+      refresh:
+        (value.builder !== acc.previousBuilder &&
+          value.builder === BuildState.Finished) ||
+        value.gateway !== acc.previousGateway,
+    }),
+    {
+      previousBuilder: BuildState.Finished,
+      previousGateway: GatewayState.Ready,
+      refresh: false,
+    },
+  ),
+  filter((item) => item.refresh),
+);
+
+@customElement('publisher-refresh')
+// @ts-ignore
+export class PublisherRefresh extends LitElement {
+  @state() status: BuildState;
+  @state() outdated: boolean;
+
+  constructor() {
+    super();
+    this.outdated = false;
+    this.status = BuildState.Init;
+    refreshSignal$.subscribe(() => {
+      this.outdated = true;
+      window.location.reload();
+    });
+    updates$.subscribe((value) => {
+      this.status = value.builder;
+    });
+  }
+
+  static styles = style;
+
+  protected render() {
+    const label = {
+      [BuildState.Init]: 'Initializing ...',
+      [BuildState.Finished]: 'Up-to-date',
+      [BuildState.Failed]: 'Failed',
+      [BuildState.Running]: 'Building ...',
+    }[this.status];
+
+    const statusStyle = {
+      [BuildState.Init]: 'init',
+      [BuildState.Finished]: 'finished',
+      [BuildState.Failed]: 'failed',
+      [BuildState.Running]: 'running',
+    }[this.status];
+
+    return html`<div class="status ${statusStyle}">${label}</div>`;
+  }
+}
