4f5365bbd55f12e6d1e7d57e9adb83331a1f779c
Philipp Melab <philipp.melab@gmail.com>
2022-03-13 17:06:42 +0100

    feature(gatsby): add `useLocation` hook that allows to receive navigation events in stories

--- packages/npm/@amazeelabs/react-framework-bridge/src/__tests__/storybook.test.tsx
+++ packages/npm/@amazeelabs/react-framework-bridge/src/__tests__/storybook.test.tsx
@@ -2,12 +2,15 @@ import { fireEvent, render, screen, waitFor } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import { Field, FormikValues } from 'formik';
 import React, { useEffect, useState } from 'react';
+import { act } from 'react-dom/test-utils';
 
 import {
   ActionsDecorator,
   buildForm,
   buildImage,
   buildLink,
+  createLocation,
+  useLocation,
 } from '../storybook';
 
 const action = jest.fn();
@@ -20,6 +23,19 @@ jest.mock('@storybook/addon-actions', () => ({
 }));
 
 beforeEach(jest.resetAllMocks);
+
+describe('createLocation', () => {
+  test('creates a relative url', () => {
+    const location = createLocation('/foo?a=b#bar');
+    expect(location.params.has('a')).toBeTruthy();
+    expect(location.params.get('a')).toEqual('b');
+    expect(location).toEqual({
+      pathname: '/foo',
+      params: new URLSearchParams('?a=b'),
+      hash: 'bar',
+    });
+  });
+});
 
 describe('buildLink', () => {
   it('can build a link from segments and query parameters', () => {
@@ -134,7 +150,9 @@ describe('buildLink', () => {
         args: { wouldNavigate },
       } as any),
     );
-    fireEvent.click(screen.getByRole('link'));
+    act(() => {
+      fireEvent.click(screen.getByRole('link'));
+    });
     expect(wouldNavigate).toHaveBeenCalledTimes(1);
     expect(wouldNavigate).toHaveBeenCalledWith('#test');
   });
@@ -152,7 +170,9 @@ describe('buildLink', () => {
         } as any,
       ),
     );
-    Link.navigate();
+    act(() => {
+      Link.navigate();
+    });
     expect(wouldNavigate).toHaveBeenCalledTimes(1);
     expect(wouldNavigate).toHaveBeenCalledWith('#test');
   });
@@ -170,9 +190,56 @@ describe('buildLink', () => {
         } as any,
       ),
     );
-    Link.navigate({ query: { a: 'c' }, fragment: 'bar' });
+    act(() => {
+      Link.navigate({ query: { a: 'c' }, fragment: 'bar' });
+    });
     expect(wouldNavigate).toHaveBeenCalledTimes(1);
     expect(wouldNavigate).toHaveBeenCalledWith('/foo?a=c#bar');
+  });
+
+  it('exposes the changing locations with the `useLocation` hook', () => {
+    const LinkA = buildLink({ href: '/foo', query: { a: 'b' } });
+    const LinkB = buildLink({ href: '/bar', query: { a: 'c' } });
+    const locationTest = jest.fn();
+
+    function Test() {
+      const location = useLocation();
+      useEffect(() => {
+        locationTest(location);
+      }, [location]);
+      return (
+        <div>
+          <LinkA fragment="x">A</LinkA>
+          <LinkB fragment="y">B</LinkB>
+        </div>
+      );
+    }
+    render(
+      ActionsDecorator(() => <Test />, {
+        args: { wouldNavigate },
+      } as any),
+    );
+
+    act(() => {
+      fireEvent.click(screen.getByRole('link', { name: 'A' }));
+    });
+
+    act(() => {
+      fireEvent.click(screen.getByRole('link', { name: 'B' }));
+    });
+
+    expect(locationTest).toHaveBeenCalledTimes(3);
+    expect(locationTest).toHaveBeenNthCalledWith(1, undefined);
+    expect(locationTest).toHaveBeenNthCalledWith(2, {
+      pathname: '/foo',
+      params: new URLSearchParams('?a=b'),
+      hash: 'x',
+    });
+    expect(locationTest).toHaveBeenNthCalledWith(3, {
+      pathname: '/bar',
+      params: new URLSearchParams('?a=c'),
+      hash: 'y',
+    });
   });
 });
 
