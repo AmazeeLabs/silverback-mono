2e034fbb1c6d22c24d85b06fd3fc0600a1854657
Vasile Chindris <vasile.chindris@amazee.com>
2022-04-07 14:17:53 +0300

    chore: script to push the translations source to Drupal.

--- apps/silverback-gatsby/scripts/push_translation_sources.ts
+++ apps/silverback-gatsby/scripts/push_translation_sources.ts
@@ -1,6 +1,7 @@
 import axios from 'axios';
+import { existsSync,readFileSync } from 'fs';
 
-const pushTranslationSources = async () => {
+const pushTranslationSources = async (translationSourcesPath) => {
   if (
     !process.env.DRUPAL_BASE_URL ||
     !process.env.DRUPAL_CREATE_TRANSLATIONS_SOURCE_PATH ||
@@ -10,11 +11,17 @@ const pushTranslationSources = async () => {
     console.warn('Warning: Drupal env vars are not set.');
     return;
   }
+
+  if (!existsSync(translationSourcesPath)) {
+    console.warn('Warning: the translations sources file does not exist. Please make sure you specify a valid path and make sure you run the translations:extract yarn command before.')
+    return;
+  }
   const url = `${process.env.DRUPAL_BASE_URL}${process.env.DRUPAL_CREATE_TRANSLATIONS_SOURCE_PATH}`;
+  const translationSources = readFileSync(translationSourcesPath, {encoding: 'utf-8'});
   try {
     await axios.post(
       url,
-      '{}',
+      translationSources,
       {
         auth: {
           username: process.env.DRUPAL_USER_NAME,
@@ -34,4 +41,4 @@ const pushTranslationSources = async () => {
   }
 }
 
-pushTranslationSources();
+pushTranslationSources(process.argv[2]);
