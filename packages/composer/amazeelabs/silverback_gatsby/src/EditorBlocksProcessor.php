0aa6c7fcb3b68b03d9145903eb803f8f120ab4e5
Philipp Melab <philipp.melab@gmail.com>
2022-05-10 15:46:59 +0200

    fix(gutenberg): ignore blocks without a blockName
    can happen when there are multiple \r\n character in the document

--- packages/composer/amazeelabs/silverback_gatsby/src/EditorBlocksProcessor.php
+++ packages/composer/amazeelabs/silverback_gatsby/src/EditorBlocksProcessor.php
@@ -4,7 +4,7 @@
 class EditorBlocksProcessor {
   static function processsIgnoredBlocks(array $blocks, array $ignored) {
     $processed = [];
-    foreach ($blocks as $block) {
+    foreach (array_filter($blocks, fn ($block) => !!$block['blockName']) as $block) {
       if (in_array($block['blockName'], $ignored)) {
         foreach ((static::processsIgnoredBlocks($block['innerBlocks'] ?? [], $ignored)) as $child) {
           $processed[] = $child;
