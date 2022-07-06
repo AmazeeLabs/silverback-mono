c44f3e9eb6210b8eb210789e008fc77a7f5c5204
Christophe Jossart <christophe@colorfield.eu>
2022-07-06 11:14:05 +0200

    fix: run tests in isolation, use mock-fs, use async if generate uses async read/write

--- packages/npm/@amazeelabs/gatsby-fragments/src/generate.test.ts
+++ packages/npm/@amazeelabs/gatsby-fragments/src/generate.test.ts
@@ -1,13 +1,42 @@
 import { readFileSync } from 'fs';
 import { sync } from 'glob';
+import mock from 'mock-fs';
 
 import { generate } from './generate';
 
-describe('generate', () => {
-  const filesAmountEquality = () => sync('./test/*.gql').length === sync('./test/*.fragment.ts').length;
+beforeEach(mock.restore);
 
-  const typeScriptFilesContains = (pattern:string) => {
-    const files = sync('./test/*.ts');
+describe('generate', () => {
+  const fragmentsPath = '/fragments';
+  const gqlMocks = {
+    '/fragments': {
+      'ContentArticle.gql': `fragment ContentBlog on ContentBlog {
+        __typename
+        title
+        headerImage {
+          ...MediaImage
+        }
+      }`,
+      'ContentPage.gql': `fragment ContentPage on ContentPage {
+        __typename
+        title
+        articleReference {
+          ... on ContentArticle {
+            title
+          }
+        }
+      }`,
+      'MediaImage.gql': `fragment MediaImage on MediaImage {
+        __typename
+        url
+      }`,
+    },
+  };
+
+  const filesAmountEquality = (path:string) => sync(`${path}/*.gql`).length === sync(`${path}/*.fragment.ts`).length;
+
+  const typeScriptFilesContains = (path: string, pattern:string) => {
+    const files = sync(`${path}/*.ts`);
     let result = true;
     for (const filePath of files) {
       const content = readFileSync(filePath, {encoding: 'utf-8'});
@@ -22,17 +51,21 @@ describe('generate', () => {
     );
   });
 
-  generate({ path: './test' });
-
-  it('same amount of source and generated files', () => {
-    expect(filesAmountEquality()).toBe(true)
+  it('same amount of source and generated files', async () => {
+    mock(gqlMocks);
+    await generate({ path: fragmentsPath });
+    expect(filesAmountEquality(fragmentsPath)).toBe(true);
   });
 
-  it('all typescript fragments contains original typename', () => {
-    expect(typeScriptFilesContains('__typename:_original_typename')).toBe(true)
+  it('all typescript fragments contains original typename', async () => {
+    mock(gqlMocks);
+    await generate({ path: fragmentsPath });
+    expect(typeScriptFilesContains(fragmentsPath, '__typename:_original_typename')).toBe(true);
   });
 
-  it('all typescript fragments contains Drupal prefix', () => {
-    expect(typeScriptFilesContains('on Drupal')).toBe(true)
+  it('all typescript fragments contains Drupal prefix', async () => {
+    mock(gqlMocks);
+    await generate({ path: fragmentsPath });
+    expect(typeScriptFilesContains(fragmentsPath, 'on Drupal')).toBe(true);
   });
 });
