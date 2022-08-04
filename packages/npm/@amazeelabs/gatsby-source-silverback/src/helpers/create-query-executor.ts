862a01b679bdbe591aa4c4f26d2ed6331dadb8f2
Alex Tkachev <alex.leksat@gmail.com>
2022-08-04 12:09:07 +0300

    feat: improved error handling for graphql requests

--- packages/npm/@amazeelabs/gatsby-source-silverback/src/helpers/create-query-executor.ts
+++ packages/npm/@amazeelabs/gatsby-source-silverback/src/helpers/create-query-executor.ts
@@ -1,5 +1,7 @@
-import { createDefaultQueryExecutor } from 'gatsby-graphql-source-toolkit';
-import { RequestInit } from 'node-fetch';
+import { wrapQueryExecutorWithQueue } from 'gatsby-graphql-source-toolkit';
+import { IQueryExecutor } from 'gatsby-graphql-source-toolkit/dist/types';
+import fetch, { RequestInit } from 'node-fetch';
+import { inspect } from 'util';
 
 export const createQueryExecutor = (
   url: string,
@@ -8,7 +10,7 @@ export const createQueryExecutor = (
   authKey?: string,
   headers?: RequestInit['headers'],
 ) => {
-  return createDefaultQueryExecutor(url, {
+  const executor = createNetworkQueryExecutor(url, {
     headers: {
       ...headers,
       ...(authUser && authPass
@@ -22,4 +24,55 @@ export const createQueryExecutor = (
     },
     timeout: 60_000,
   });
+  return wrapQueryExecutorWithQueue(executor, { concurrency: 10 });
 };
+
+// Copy of the default network query executor from gatsby-graphql-source-toolkit
+// package with improved error handling.
+// https://github.com/gatsbyjs/gatsby-graphql-toolkit/blob/7ba8b24d8524254dc6c2853d6d014070befd9aea/src/config/query-executor.ts#L6-L39
+export function createNetworkQueryExecutor(
+  uri: string,
+  fetchOptions: RequestInit = {},
+): IQueryExecutor {
+  return async function execute(args) {
+    const { query, variables, operationName } = args;
+
+    let response;
+    try {
+      response = await fetch(uri, {
+        method: 'POST',
+        body: JSON.stringify({ query, variables, operationName }),
+        ...fetchOptions,
+        headers: {
+          'Content-Type': 'application/json',
+          ...fetchOptions.headers,
+        },
+      });
+    } catch (e) {
+      console.warn(
+        `Query ${operationName} failed.\n` +
+          `Query variables: ${inspect(variables)}\n` +
+          `Full query: ${inspect(query)}\n`,
+      );
+      throw e;
+    }
+    if (!response.ok) {
+      console.warn(
+        `Query ${operationName} returned status ${response.status}.\n` +
+          `Query variables: ${inspect(variables)}\n` +
+          `Full query: ${inspect(query)}\n`,
+      );
+    }
+    const result = await response.json();
+
+    if (result.data && result.errors?.length) {
+      console.warn(
+        `Query ${operationName} returned warnings:\n` +
+          `${inspect(result.errors)}\n` +
+          `Query variables: ${inspect(variables)}\n` +
+          `Full query: ${inspect(query)}\n`,
+      );
+    }
+    return result;
+  };
+}
