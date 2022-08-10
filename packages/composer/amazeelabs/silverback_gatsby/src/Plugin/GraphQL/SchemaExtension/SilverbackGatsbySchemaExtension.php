6443f6c7d6f12a78699fbd0cae056cb313e4672d
Alex Tkachev <alex.leksat@gmail.com>
2022-08-10 12:35:19 +0300

    fix: remove dots-only path segments

--- packages/composer/amazeelabs/silverback_gatsby/src/Plugin/GraphQL/SchemaExtension/SilverbackGatsbySchemaExtension.php
+++ packages/composer/amazeelabs/silverback_gatsby/src/Plugin/GraphQL/SchemaExtension/SilverbackGatsbySchemaExtension.php
@@ -504,21 +504,25 @@ class SilverbackGatsbySchemaExtension extends SdlSchemaExtensionPluginBase
                 // Gatsby expects raw paths. Not encoded.
                 $path = rawurldecode($path);
 
-                // Some characters may ruin Gatsby.
-                // See https://github.com/gatsbyjs/gatsby/discussions/36345
-                $bannedCharacters = [
-                  // @gatsbyjs/reach-router applies decodeURIComponent function
-                  // to paths. And % sign can lead to "URI malformed" error.
-                  '%',
-                  // Other dangerous characters. The list is maybe incomplete.
-                  '?', '#', '\\',
-                ];
+                // Some paths may ruin Gatsby.
+                // See https://github.com/gatsbyjs/gatsby/discussions/36345#discussioncomment-3364844
                 $original = $path;
+                $bannedCharacters = ['%', '?', '#', '\\'];
                 foreach ($bannedCharacters as $character) {
                   if (strpos($path, $character) !== FALSE) {
                     $path = str_replace($character, '', $path);
-                    $this->logger->warning("Found entity with '{$character}' character in its path alias. The character will not be removed from the Gatsby page path. This can lead to broken links. Please update the entity path alias. Original path alias: '{$original}'.");
+                    $this->logger->warning("Found entity with '{$character}' character in its path alias. The character will be removed from the Gatsby page path. This can lead to broken links. Please update the entity path alias. Original path alias: '{$original}'.");
                   }
+                }
+                $dotsOnlyRegex = '/^\.+$/';
+                $pathSegments = explode('/', $path);
+                $pathSegmentsFiltered = array_filter(
+                  $pathSegments,
+                  fn (string $segment) => !preg_match($dotsOnlyRegex, $segment)
+                );
+                if (count($pathSegments) !== count($pathSegmentsFiltered)) {
+                  $path = implode('/', $pathSegmentsFiltered);
+                  $this->logger->warning("Found entity with dots-only path segment(s) in its path alias. These segments will be removed from the Gatsby page path. This can lead to broken links. Please update the entity path alias. Original path alias: '{$original}'.");
                 }
 
                 return $path;
