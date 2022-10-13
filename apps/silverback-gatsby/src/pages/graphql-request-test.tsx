dc5cd41ef01d2d7760e62c068acfb0c8a25e5b5d
Philipp Melab <philipp.melab@gmail.com>
2022-10-13 12:21:33 +0200

    refactor(gatsby): use new persisted query plugin in react-query and graphql request tests

--- apps/silverback-gatsby/src/pages/graphql-request-test.tsx
+++ apps/silverback-gatsby/src/pages/graphql-request-test.tsx
@@ -1,23 +1,63 @@
-import { withPersistedQueries } from '@amazeelabs/silverback-graphql-persisted';
-import { GraphQLClient } from 'graphql-request';
 import React, { useEffect, useState } from 'react';
 
-import { GetPagesQuery, getSdk } from '../../generated/graphql-request';
-import queryMap from '../../generated/persisted-queries-map.json';
+import {
+  AnyOperationId,
+  GetPagesQuery,
+  GetRandomIntMutation,
+  OperationResult,
+  OperationVariables,
+} from '../../generated/operations';
+
+async function graphqlFetch<T extends AnyOperationId>(
+  id: T,
+  variables: OperationVariables<T>,
+  method: 'GET' | 'POST' = 'GET',
+): Promise<OperationResult<T>> {
+  if (method === 'GET') {
+    const url = new URL(process.env.GATSBY_GRAPHQL_ENDPOINT!);
+    url.searchParams.set('id', id);
+    url.searchParams.set('variables', JSON.stringify(variables));
+    return (
+      await (
+        await fetch(url.toString(), {
+          credentials: 'include',
+        })
+      ).json()
+    ).data;
+  } else {
+    return await (
+      await (
+        await fetch(process.env.GATSBY_GRAPHQL_ENDPOINT!, {
+          method: 'POST',
+          body: JSON.stringify({ id, variables }),
+          credentials: 'include',
+        })
+      ).json()
+    ).data;
+  }
+}
 
 const Sitemap: React.FC = () => {
-  const sdk = getSdk(
-    new GraphQLClient(process.env.GATSBY_GRAPHQL_ENDPOINT!, {
-      fetch: withPersistedQueries(fetch, queryMap),
-    }),
-  );
   const [randomInt, setRandomInt] = useState<number>();
-  const [pages, setPages] = useState<GetPagesQuery['queryPages']>();
+  const [pages, setPages] =
+    useState<OperationResult<typeof GetPagesQuery>['queryPages']>();
   useEffect(() => {
     (async () => {
-      setPages((await sdk.GetPages({ offset: 0, limit: 2 })).queryPages);
+      setPages(
+        (await graphqlFetch(GetPagesQuery, { offset: 0, limit: 2 })).queryPages,
+      );
       const fetchRandomInt = async () => {
-        setRandomInt((await sdk.GetRandomInt({ randomInt: 123 })).getRandomInt);
+        setRandomInt(
+          (
+            await graphqlFetch(
+              GetRandomIntMutation,
+              {
+                randomInt: 123,
+              },
+              'POST',
+            )
+          ).getRandomInt,
+        );
       };
       await fetchRandomInt();
       setInterval(fetchRandomInt, 1_000);
--- apps/silverback-gatsby/src/pages/react-query-test.tsx
+++ apps/silverback-gatsby/src/pages/react-query-test.tsx
@@ -1,15 +1,78 @@
 import React, { useEffect, useState } from 'react';
+import {
+  useMutation,
+  UseMutationOptions,
+  useQuery,
+  UseQueryOptions,
+} from 'react-query';
 
 import {
-  useGetPagesQuery,
-  useGetRandomIntMutation,
-} from '../../generated/react-query';
+  AnyOperationId,
+  GetPagesQuery,
+  GetRandomIntMutation,
+  OperationResult,
+  OperationVariables,
+} from '../../generated/operations';
+
+function usePersistedQuery<T extends AnyOperationId>(
+  id: T,
+  variables: OperationVariables<T>,
+  options?: Omit<
+    UseQueryOptions<OperationResult<T>, any, OperationResult<T>, any>,
+    'queryKey' | 'queryFn'
+  >,
+) {
+  return useQuery<OperationResult<T>>(
+    [id, variables],
+    async () => {
+      const url = new URL(process.env.GATSBY_GRAPHQL_ENDPOINT!);
+      url.searchParams.set('id', id);
+      url.searchParams.set('variables', JSON.stringify(variables));
+      return (
+        await (
+          await fetch(url.toString(), {
+            credentials: 'include',
+          })
+        ).json()
+      ).data;
+    },
+    options,
+  );
+}
+
+function usePersistedMutation<T extends AnyOperationId>(
+  id: T,
+  options: UseMutationOptions<
+    OperationResult<T>,
+    unknown,
+    OperationVariables<T>
+  >,
+) {
+  return useMutation<OperationResult<T>, any, OperationVariables<T>>(
+    async (variables) => {
+      return (
+        await (
+          await fetch(process.env.GATSBY_GRAPHQL_ENDPOINT!, {
+            method: 'POST',
+            body: JSON.stringify({ id, variables }),
+            credentials: 'include',
+          })
+        ).json()
+      ).data;
+    },
+    options,
+  );
+}
 
 const Sitemap: React.FC = () => {
   const [randomInt, setRandomInt] = useState<number>();
-  const { data: pages } = useGetPagesQuery({ offset: 0, limit: 2 });
-  const { mutate } = useGetRandomIntMutation({
+  const { data } = usePersistedQuery(GetPagesQuery, {
+    offset: 0,
+    limit: 2,
+  });
+  const { mutate } = usePersistedMutation(GetRandomIntMutation, {
     onSuccess: (data) => {
+      console.log(data);
       setRandomInt(data.getRandomInt);
     },
   });
@@ -23,7 +86,7 @@ const Sitemap: React.FC = () => {
       <h1>React Query test</h1>
       <div>
         Amount of loaded pages (should be 2):{' '}
-        <span id="pages-count">{pages?.queryPages.length}</span>
+        <span id="pages-count">{data?.queryPages.length}</span>
       </div>
       <div>
         Loaded random int (should change):{' '}
