a6578e46a8b8da606ad00f11302b6caba2b8fe50
Philipp Melab <philipp.melab@gmail.com>
2022-10-18 10:07:12 +0200

    refactor(mzx): improve test case readability

--- packages/npm/@amazeelabs/mzx/src/extract.test.ts
+++ packages/npm/@amazeelabs/mzx/src/extract.test.ts
@@ -2,77 +2,73 @@ import { describe, expect, it } from 'vitest';
 
 import { extractCodeBlocks } from './extract';
 
+function assertCodeBlocks(input: Array<string>, output: Array<string>) {
+  expect(extractCodeBlocks(input.join('\n'))).toEqual(output.join('\n'));
+}
+
 describe('extractCodeBlocks', () => {
   it('returns nothing if there are no code blocks', () => {
-    expect(
-      extractCodeBlocks(`
-# Hello there!
-    `),
-    ).toEqual('');
+    assertCodeBlocks(['# Hello there'], []);
   });
 
   it('returns a file with all code blocks in known languages', () => {
-    expect(
-      extractCodeBlocks(`
-# Hello there!
-
-\`\`\`typescript
-await $\`pwd\`
-\`\`\`
-
-Something else
-
-\`\`\`yaml
-foo: bar
-\`\`\`
-    `),
-    ).toEqual('await $`pwd`');
+    assertCodeBlocks(
+      [
+        '# Hello there, i am a executable script',
+        '',
+        '```typescript',
+        'await $`pwd`',
+        '```',
+      ],
+      ['await $`pwd`'],
+    );
   });
 
   it('turns shell script blocks into zx shell commands', () => {
-    expect(
-      extractCodeBlocks(`
-# Hello there!
-
-\`\`\`shell
-mkdir test
-cd test
-yarn init -y
-\`\`\`
-    `),
-    ).toEqual(
-      'await $`mkdir test`;\nawait $`cd test`;\nawait $`yarn init -y`;',
+    assertCodeBlocks(
+      [
+        '# Hello there!',
+        '',
+        '```shell',
+        'mkdir test',
+        'cd test',
+        'yarn init -y',
+        '```',
+      ],
+      ['await $`mkdir test`;', 'await $`cd test`;', 'await $`yarn init -y`;'],
     );
   });
 
   it('writes annotated code blocks as files', () => {
-    expect(
-      extractCodeBlocks(`
-# Hello there!
-
-\`\`\`yaml
-# |-> config/test.yaml
-foo: bar
-bar: baz
-\`\`\`
-    `),
-    ).toEqual("await fs.writeFile('config/test.yaml', 'foo: bar\nbar: baz');");
+    assertCodeBlocks(
+      [
+        '# Hello there!',
+        '',
+        '```yaml',
+        '# |-> config/test.yaml',
+        'foo: bar',
+        'bar: baz',
+        '```',
+      ],
+      ["await fs.writeFile('config/test.yaml', 'foo: bar\nbar: baz');"],
+    );
   });
 
   it('interpolates upper-cased environment variables into files and filenames', () => {
     process.env.PROJECT_NAME = 'my_project';
-    expect(
-      extractCodeBlocks(`
-# Hello there!
-
-\`\`\`yaml
-# |-> config/PROJECT_NAME.yaml
-foo: bar
-bar: PROJECT_NAME
-\`\`\`
-    `),
-    ).toEqual(
-      "await fs.writeFile('config/' + (process.env.PROJECT_NAME || 'PROJECT_NAME') + '.yaml', 'foo: bar\nbar: ' + (process.env.PROJECT_NAME || 'PROJECT_NAME') + '');",
+    assertCodeBlocks(
+      [
+        '# Hello there!',
+        '',
+        '```yaml',
+        '# |-> config/PROJECT_NAME.yaml',
+        'foo: bar',
+        'bar: PROJECT_NAME',
+        '```',
+      ],
+      [
+        "await fs.writeFile('config/' + (process.env.PROJECT_NAME || 'PROJECT_NAME') + '.yaml', 'foo: bar\nbar: ' + (process.env.PROJECT_NAME || 'PROJECT_NAME') + '');",
+      ],
     );
   });
 });
