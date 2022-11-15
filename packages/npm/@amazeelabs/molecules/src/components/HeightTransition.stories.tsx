119fee703a697141cc48b71efc93a55ca6abf695
Philipp Melab <philipp.melab@gmail.com>
2022-11-15 14:01:39 +0100

    feat(molecules): fundamental height transition

--- packages/npm/@amazeelabs/molecules/src/components/HeightTransition.stories.tsx
+++ packages/npm/@amazeelabs/molecules/src/components/HeightTransition.stories.tsx
@@ -1,44 +1,68 @@
-import {ComponentStoryObj, DecoratorFn, Meta} from '@storybook/react';
+import { Meta } from '@storybook/react';
+import { useState } from 'react';
 
-import {HeightTransition} from "./HeightTransition";
-
+import { HeightTransition } from './HeightTransition';
 
 export default {
   title: 'Components/HeightTransition',
   component: HeightTransition,
 } as Meta;
 
-const ContentContainer: DecoratorFn = (story) => (
-    <>
-      <p className={'text-lg my-10'}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur</p>
-      <p className={'text-lg my-10'}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur</p>
-      <p className={'text-lg my-10'}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur</p>
-      <p className={'text-lg my-10'}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur</p>
-      <p className={'text-lg my-10'}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur</p>
-      <div className={'my-20'}>
-        {story()}
-      </div>
-      <p className={'text-lg my-10'}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur</p>
-      <p className={'text-lg my-10'}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur</p>
-      <p className={'text-lg my-10'}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur</p>
-      <p className={'text-lg my-10'}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur</p>
-      <p className={'text-lg my-10'}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur</p>
-      <p className={'text-lg my-10'}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur</p>
-
-    </>
+const content = (
+  <div className={'text-lg bg-indigo-200 p-2'}>
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+    quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+    consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+    cillum dolore eu fugiat nulla pariatur
+  </div>
 );
 
-export const Default: ComponentStoryObj<typeof HeightTransition> = {
-  decorators: [ContentContainer],
-  args: {
-      show: true,
-    children: <div>
-      <p className={'text-lg my-10'}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur</p>
-      <p className={'text-lg my-10'}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur</p>
-      <p className={'text-lg my-10'}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur</p>
-      <p className={'text-lg my-10'}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur</p>
-      <p className={'text-lg my-10'}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur</p>
-      <p className={'text-lg my-10'}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur</p>
-    </div>
-  },
-};
+export function Open() {
+  const [open, setOpen] = useState(true);
+  return (
+    <>
+      <button onClick={() => setOpen(!open)}>Toggle</button>
+      <HeightTransition show={open}>{content}</HeightTransition>
+    </>
+  );
+}
+
+export function Closed() {
+  const [open, setOpen] = useState(false);
+  return (
+    <>
+      <button onClick={() => setOpen(!open)}>Toggle</button>
+      <HeightTransition show={open}>{content}</HeightTransition>
+    </>
+  );
+}
+
+export function Multiple() {
+  const [openA, setOpenA] = useState(false);
+  const [openB, setOpenB] = useState(true);
+  return (
+    <>
+      <div>
+        <button onClick={() => setOpenA(!openA)}>Toggle</button>
+        <HeightTransition show={openA}>{content}</HeightTransition>
+      </div>
+      <div>
+        <button onClick={() => setOpenB(!openB)}>Toggle</button>
+        <HeightTransition show={openB}>{content}</HeightTransition>
+      </div>
+    </>
+  );
+}
+
+export function Delayed() {
+  const [open, setOpen] = useState(false);
+  return (
+    <>
+      <button onClick={() => setOpen(!open)}>Toggle</button>
+      <HeightTransition show={open} delayEnter={1} delayLeave={1}>
+        {content}
+      </HeightTransition>
+    </>
+  );
+}
--- packages/npm/@amazeelabs/molecules/src/components/HeightTransition.tsx
+++ packages/npm/@amazeelabs/molecules/src/components/HeightTransition.tsx
@@ -1,75 +1,127 @@
-import gsap, { Power3 } from 'gsap';
+import gsap from 'gsap';
 import {
-  forwardRef,
   memo,
+  MutableRefObject,
   ReactNode,
-  useEffect,
   useLayoutEffect,
   useRef,
 } from 'react';
+import { createStore, StoreApi } from 'zustand';
 
-const Animated = memo(
-  forwardRef(({ children }: { children: ReactNode }, ref) => {
-    const el = useRef<HTMLDivElement>(null);
-    const animation = useRef<gsap.core.Tween>();
-    useLayoutEffect(() => {
-      const ctx = gsap.context(() => {
-        if (el.current) {
-          animation.current = gsap.from(el.current, {
-            height: 0,
-          });
-        }
-      });
-      return () => ctx.revert();
-    }, []);
+type HeightTransitionStore = {
+  initialState?: boolean;
+  el?: HTMLDivElement;
+  tween?: gsap.core.Tween;
+  animate: (reverse: boolean) => gsap.core.Tween | undefined;
+  clean: () => void;
+  setOpened: (opened: boolean) => void;
+  setElement: (el: HTMLDivElement) => void;
+};
 
-    useEffect(() => {
-      // forward the animation instance
-      if (typeof ref === 'function') {
-        ref(animation.current);
-      } else if (ref) {
-        ref.current = animation.current;
-      }
-    }, [ref]);
+const Animated = memo(function Animation({
+  children,
+  storeRef,
+}: {
+  children: ReactNode;
+  storeRef: MutableRefObject<StoreApi<HeightTransitionStore>>;
+}) {
+  const el = useRef<HTMLDivElement>(null);
+  useLayoutEffect(() => {
+    const store = storeRef.current;
+    if (el.current) {
+      store.getState().setElement(el.current);
+    }
+    return () => store.getState().clean();
+  }, [storeRef]);
 
-    return (
-      <div ref={el} className={'bg-red-500'}>
-        {children}
-      </div>
-    );
-  }),
-);
+  return (
+    <div
+      ref={el}
+      style={{
+        overflow: 'hidden',
+      }}
+    >
+      {children}
+    </div>
+  );
+});
 
 export function HeightTransition({
   children,
   show,
-  durationEnter = 0.5,
-  durationLeave = 0.5,
+  duration = 0.5,
   beforeEnter,
   afterEnter,
   beforeLeave,
   afterLeave,
   delayEnter = 0,
   delayLeave = 0,
-  className,
 }: {
   children?: ReactNode;
   show: boolean;
-  durationEnter?: number;
-  durationLeave?: number;
+  duration?: number;
   beforeEnter?: () => void;
   afterEnter?: () => void;
   beforeLeave?: () => void;
   afterLeave?: () => void;
   delayEnter?: number;
   delayLeave?: number;
-  className?: string;
 }) {
-  const animation = useRef<gsap.core.Tween>();
+  const storeRef = useRef<StoreApi<HeightTransitionStore>>(undefined!);
+  if (storeRef.current === undefined) {
+    storeRef.current = createStore<HeightTransitionStore>((set, getState) => {
+      return {
+        setElement: (el) => {
+          set({ el });
+        },
+        animate: (reverse) => {
+          const el = getState().el;
+          if (el) {
+            const tween = getState().tween;
+            if (tween) {
+              tween.reversed(reverse);
+              return tween;
+            }
+            const newTween = getState().initialState
+              ? gsap.to(el, {
+                  height: 0,
+                  duration: duration,
+                })
+              : gsap.to(el, {
+                  height: 'auto',
+                  duration: duration,
+                });
+            set({ tween: newTween });
+            return newTween;
+          }
+        },
+        clean: () => {
+          const tween = getState().tween;
+          if (tween) {
+            tween.revert();
+            set({ tween: undefined });
+          }
+        },
+        setOpened: (opened) => {
+          if (getState().initialState === undefined) {
+            const el = getState().el;
+            if (el) {
+              el.style.height = opened ? 'auto' : '0';
+              set({ initialState: opened });
+            }
+          } else {
+            (opened ? beforeEnter : beforeLeave)?.();
+            getState()
+              .animate(getState().initialState === opened)
+              ?.then(opened ? afterEnter : afterLeave);
+          }
+        },
+      };
+    });
+  }
+  useLayoutEffect(() => {
+    storeRef.current?.getState().setOpened(show);
+  }, [storeRef, show]);
 
-  useEffect(() => {
-    animation.current?.reversed(show);
-  }, [show]);
-
-  return <Animated ref={animation}>{children}</Animated>;
+  return <Animated storeRef={storeRef}>{children}</Animated>;
 }
