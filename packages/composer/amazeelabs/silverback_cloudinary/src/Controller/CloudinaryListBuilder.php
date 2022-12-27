0e95a25b3954dab24db9f3fa97d4216f97b1fcaa
Vasile Chindris <vasile.chindris@amazee.com>
2022-12-27 16:10:48 +0200

    feat: add the missing settings for the clodinary config entity

--- packages/composer/amazeelabs/silverback_cloudinary/src/Controller/CloudinaryListBuilder.php
+++ packages/composer/amazeelabs/silverback_cloudinary/src/Controller/CloudinaryListBuilder.php
@@ -4,15 +4,17 @@ namespace Drupal\silverback_cloudinary\Controller;
 
 use Drupal\Core\Config\Entity\ConfigEntityListBuilder;
 use Drupal\Core\Entity\EntityInterface;
+use Drupal\silverback_cloudinary\Entity\Cloudinary;
 
 class CloudinaryListBuilder extends ConfigEntityListBuilder {
-
   /**
    * {@inheritdoc}
    */
   public function buildHeader() {
     $header['label'] = $this->t('Label');
     $header['id'] = $this->t('Machine name');
+    $header['cloud_name'] = $this->t('Cloud name');
+    $header['default'] = $this->t('Default');
     return $header + parent::buildHeader();
   }
 
@@ -20,8 +22,11 @@ class CloudinaryListBuilder extends ConfigEntityListBuilder {
    * {@inheritdoc}
    */
   public function buildRow(EntityInterface $entity) {
+    $defaultInstance = Cloudinary::getDefaultInstance();
     $row['label'] = $entity->label();
     $row['id'] = $entity->id();
+    $row['cloud_name'] = $entity->getCloudName();
+    $row['default'] = $defaultInstance === $entity->id() ? $this->t('Yes') : $this->t('No');
 
     return $row + parent::buildRow($entity);
   }
--- packages/composer/amazeelabs/silverback_cloudinary/src/Form/CloudinaryForm.php
+++ packages/composer/amazeelabs/silverback_cloudinary/src/Form/CloudinaryForm.php
@@ -3,30 +3,9 @@
 namespace Drupal\silverback_cloudinary\Form;
 
 use Drupal\Core\Entity\EntityForm;
-use Drupal\Core\Entity\EntityTypeManagerInterface;
 use Drupal\Core\Form\FormStateInterface;
-use Symfony\Component\DependencyInjection\ContainerInterface;
 
 class CloudinaryForm extends EntityForm {
-
-  /**
-   * Constructs an CloudinaryForm object.
-   *
-   * @param \Drupal\Core\Entity\EntityTypeManagerInterface $entityTypeManager
-   *   The entityTypeManager.
-   */
-  public function __construct(EntityTypeManagerInterface $entityTypeManager) {
-    $this->entityTypeManager = $entityTypeManager;
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public static function create(ContainerInterface $container) {
-    return new static(
-      $container->get('entity_type.manager')
-    );
-  }
 
   /**
    * {@inheritdoc}
@@ -34,6 +13,7 @@ class CloudinaryForm extends EntityForm {
   public function form(array $form, FormStateInterface $form_state) {
     $form = parent::form($form, $form_state);
 
+    /* @var \Drupal\silverback_cloudinary\CloudinaryInterface $cloudinary */
     $cloudinary = $this->entity;
 
     $form['label'] = [
@@ -52,6 +32,34 @@ class CloudinaryForm extends EntityForm {
       ],
       '#disabled' => !$cloudinary->isNew(),
     ];
+    $form['is_default'] = [
+      '#type' => 'checkbox',
+      '#title' => $this->t('Default instance'),
+      '#default_value' => $cloudinary->isDefault(),
+    ];
+
+    $form['cloud_name'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('Cloud name'),
+      '#description' => $this->t('The cloud name you can find on your cloudinary dashboard.'),
+      '#default_value' => $cloudinary->getCloudName(),
+      '#required' => TRUE,
+    ];
+
+    $form['api_key'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('API Key'),
+      '#description' => $this->t('The API Key you can find on your cloudinary dashboard.'),
+      '#default_value' => $cloudinary->getApiKey(),
+      '#required' => TRUE,
+    ];
+    $form['api_secret'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('API Secret'),
+      '#description' => $this->t('The API Secret you can find on your cloudinary dashboard.'),
+      '#default_value' => $cloudinary->getApiSecret(),
+      '#required' => TRUE,
+    ];
 
     return $form;
   }
@@ -60,6 +68,7 @@ class CloudinaryForm extends EntityForm {
    * {@inheritdoc}
    */
   public function save(array $form, FormStateInterface $form_state) {
+    /* @var \Drupal\silverback_cloudinary\CloudinaryInterface $cloudinary */
     $cloudinary = $this->entity;
     $status = $cloudinary->save();
 
@@ -72,6 +81,14 @@ class CloudinaryForm extends EntityForm {
       $this->messenger()->addMessage($this->t('The %label Cloudinary instance has been updated.', [
         '%label' => $cloudinary->label(),
       ]));
+    }
+
+    // Update the default instance.
+    if ($cloudinary->isDefault()) {
+      $cloudinary->setDefault(FALSE);
+    }
+    if (!empty($form_state->getValue('is_default'))) {
+      $cloudinary->setDefault();
     }
 
     $form_state->setRedirect('entity.cloudinary.collection');
