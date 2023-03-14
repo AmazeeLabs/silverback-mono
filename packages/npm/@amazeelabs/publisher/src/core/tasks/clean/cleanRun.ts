5daef1cc41d789fecc835938f17aed45fc2a7fb8
Alex Tkachev <alex.leksat@gmail.com>
2023-03-14 18:26:07 +0300

    fix: clean should remove the saved builds

--- packages/npm/@amazeelabs/publisher/src/core/tasks/clean/cleanRun.ts
+++ packages/npm/@amazeelabs/publisher/src/core/tasks/clean/cleanRun.ts
@@ -1,3 +1,5 @@
+import { fs, path } from 'zx';
+
 import { core } from '../../core';
 import { getConfig } from '../../tools/config';
 import { TaskJob } from '../../tools/queue';
@@ -9,6 +11,23 @@ export const cleanRunTask: TaskJob = async (controller) => {
   controller.onCancel(() => {
     cancelled = true;
   });
+
+  const config = getConfig().persistentBuilds;
+  if (config) {
+    core.output$.next('Removing saved builds', 'info');
+    const savedBuildsPath = path.resolve(config.saveTo);
+    try {
+      fs.removeSync(savedBuildsPath);
+    } catch (e) {
+      core.output$.next(`Failed to remove ${savedBuildsPath}`, 'error');
+      return false;
+    }
+    core.output$.next('Removed saved builds', 'info');
+  }
+  if (cancelled) {
+    return false;
+  }
+
   const process = run({ command: getConfig().commands.clean, controller });
   const { exitCode } = await process.result;
   const success = !cancelled && exitCode === 0;
