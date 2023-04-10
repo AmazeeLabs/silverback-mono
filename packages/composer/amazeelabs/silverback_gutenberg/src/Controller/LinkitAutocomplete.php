6eb9c19ba7fc97783be686ec4a9c10e7b959fe7f
Alex Tkachev <alex.leksat@gmail.com>
2023-04-10 16:48:01 +0300

    feat: complete gutenberg-linkit integration (without patches)

---
+++ packages/composer/amazeelabs/silverback_gutenberg/src/Controller/LinkitAutocomplete.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace Drupal\silverback_gutenberg\Controller;
+
+use Drupal\Component\Serialization\Json;
+use Drupal\Core\Entity\EntityInterface;
+use Drupal\gutenberg\Controller\SearchController;
+use Drupal\linkit\Controller\AutocompleteController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+
+class LinkitAutocomplete extends SearchController {
+
+  public function search(Request $request): JsonResponse {
+    $linkitProfileId = 'gutenberg';
+
+    if (
+      !\Drupal::moduleHandler()->moduleExists('linkit') ||
+      !\Drupal::entityTypeManager()->getStorage('linkit_profile')->load($linkitProfileId)
+    ) {
+      return parent::search($request);
+    }
+
+    $type = (string) $request->query->get('type');
+    if ($type !== 'post') {
+      return new JsonResponse([]);
+    }
+
+    $result = [];
+    $search = (string) $request->query->get('search');
+    $request->query->set('q', $search);
+    $linkitController = AutocompleteController::create(\Drupal::getContainer());
+    $response = $linkitController->autocomplete($request, $linkitProfileId);
+    $rows = Json::decode($response->getContent())['suggestions'] ?? [];
+    /** @var \Drupal\linkit\SubstitutionManagerInterface $substitutionManager */
+    $substitutionManager = \Drupal::service('plugin.manager.linkit.substitution');
+    /** @var \Drupal\Core\Entity\EntityRepositoryInterface $entityRepository */
+    $entityRepository = \Drupal::service('entity.repository');
+    $langcode = \Drupal::languageManager()->getCurrentLanguage()->getId();
+    foreach ($rows as $row) {
+      $url = NULL;
+      $entity = NULL;
+      if (isset($row['entity_type_id'], $row['entity_uuid'])) {
+        if ($entity = $entityRepository->loadEntityByUuid($row['entity_type_id'], $row['entity_uuid'])) {
+          $entity = $entityRepository->getTranslationFromContext($entity, $langcode);
+          if (isset($row['substitution_id'])) {
+            $url = $substitutionManager
+              ->createInstance($row['substitution_id'])
+              ->getUrl($entity)
+              ->getGeneratedUrl();
+          }
+        }
+      }
+      $result[] = [
+        'id' => $row['entity_uuid'],
+        'title' => $row['label'],
+        'type' => $entity
+          ? $this->getBundle($entity)
+          : ($row['entity_type_id'] ?? $row['description'] ?? NULL),
+        'url' => $url ?? $row['path'],
+      ];
+    }
+
+    return new JsonResponse($result);
+  }
+
+  protected function getBundle(EntityInterface $entity): string {
+    $entityType = $entity->getEntityType();
+    $bundle = NULL;
+    if ($entityType->hasKey('bundle')) {
+      $bundleKey = $entityType->getKey('bundle');
+      $bundle = $entity->get($bundleKey)->entity;
+    }
+    return $bundle
+      ? "{$entityType->getLabel()}: {$bundle->label()}"
+      : "{$entityType->getLabel()}";
+  }
+
+}
--- packages/tests/silverback-gutenberg/playwright-tests/linkit-integration.spec.ts
+++ packages/tests/silverback-gutenberg/playwright-tests/linkit-integration.spec.ts
@@ -10,10 +10,22 @@ test.beforeAll(async () => {
   await resetState();
 });
 
-test('@drupal-only linkit integration', async ({ page }) => {
+test('@drupal-only linkit content sorting', async ({ page }) => {
   await drupalLogin(page);
-  await page.goto(`${drupal.baseUrl}/en/node/add/gutenberg_page`);
 
+  const target = 'target page';
+
+  for (const title of [
+    `something else something else ${target}`,
+    `${target} something else something else`,
+    `something else ${target} something else`,
+  ]) {
+    await page.goto(`${drupal.baseUrl}/en/node/add/page`);
+    await page.getByLabel('Title', { exact: true }).fill(title);
+    await page.getByRole('button', { name: 'Save' }).click();
+  }
+
+  await page.goto(`${drupal.baseUrl}/en/node/add/gutenberg_page`);
   await page.click('[data-type="core/paragraph"]');
   await page.type('[data-type="core/paragraph"]', 'Text');
   await page.press(
@@ -21,14 +33,158 @@ test('@drupal-only linkit integration', async ({ page }) => {
     os.platform() === 'darwin' ? 'Meta+a' : 'Control+a',
   );
   await page.click('[aria-label="Link"]');
-  await page.getByPlaceholder('Search or type url').fill('ki');
-  // Linkit is configured to support media images, while the default Gutenberg
-  // autocomplete only supports nodes. Therefor, we check the list to contain
-  // the media item.
-  // Also, here we ignore the media URL.
+  await page.getByPlaceholder('Search or type url').fill(target);
+
+  await page.waitForSelector('.block-editor-link-control__search-item-title');
+  expect(
+    await page
+      .locator('.block-editor-link-control__search-item-title')
+      .allInnerTexts(),
+  ).toStrictEqual(
+    // The close the search string is to the beginning of the title, the
+    // higher the score.
+    [
+      `${target} something else something else`,
+      `something else ${target} something else`,
+      `something else something else ${target}`,
+    ],
+  );
+});
+
+test('@drupal-only linkit media sorting', async ({ page }) => {
+  await drupalLogin(page);
+
+  const target = 'target media';
+
+  for (const title of [
+    `something else something else ${target}`,
+    `${target} something else something else`,
+    `something else ${target} something else`,
+  ]) {
+    await page.goto(`${drupal.baseUrl}/en/media/add/remote_video`);
+    await page.getByLabel('Name', { exact: true }).fill(title);
+    await page
+      .getByLabel('Remote video URL', { exact: true })
+      .fill('https://www.youtube.com/watch?v=-wtAzYbklE0');
+    await page.getByRole('button', { name: 'Save' }).click();
+  }
+
+  await page.goto(`${drupal.baseUrl}/en/node/add/gutenberg_page`);
+  await page.click('[data-type="core/paragraph"]');
+  await page.type('[data-type="core/paragraph"]', 'Text');
+  await page.press(
+    '[data-type="core/paragraph"]',
+    os.platform() === 'darwin' ? 'Meta+a' : 'Control+a',
+  );
+  await page.click('[aria-label="Link"]');
+  await page.getByPlaceholder('Search or type url').fill(target);
+
+  await page.waitForSelector('.block-editor-link-control__search-item-title');
+  expect(
+    await page
+      .locator('.block-editor-link-control__search-item-title')
+      .allInnerTexts(),
+  ).toStrictEqual(
+    // The close the search string is to the beginning of the title, the
+    // higher the score.
+    [
+      `${target} something else something else`,
+      `something else ${target} something else`,
+      `something else something else ${target}`,
+    ],
+  );
+});
+
+test('@drupal-only linkit bundles', async ({ page }) => {
+  await drupalLogin(page);
+
+  const target = 'FooBar';
+
+  await page.goto(`${drupal.baseUrl}/en/node/add/page`);
+  await page.getByLabel('Title', { exact: true }).fill(`${target} page`);
+  await page.getByRole('button', { name: 'Save' }).click();
+
+  await page.goto(`${drupal.baseUrl}/en/node/add/article`);
+  await page.getByLabel('Title', { exact: true }).fill(`${target} article`);
+  await page.getByRole('button', { name: 'Save' }).click();
+
+  await page.goto(`${drupal.baseUrl}/en/media/add/remote_video`);
+  await page.getByLabel('Name', { exact: true }).fill(`${target} video`);
+  await page
+    .getByLabel('Remote video URL', { exact: true })
+    .fill('https://www.youtube.com/watch?v=-wtAzYbklE0');
+  await page.getByRole('button', { name: 'Save' }).click();
+
+  await page.goto(`${drupal.baseUrl}/en/node/add/gutenberg_page`);
+  await page.click('[data-type="core/paragraph"]');
+  await page.type('[data-type="core/paragraph"]', 'Text');
+  await page.press(
+    '[data-type="core/paragraph"]',
+    os.platform() === 'darwin' ? 'Meta+a' : 'Control+a',
+  );
+  await page.click('[aria-label="Link"]');
+  await page.getByPlaceholder('Search or type url').fill(target);
+
   await expect(
-    page.locator('.block-editor-link-control__search-item-title', {
-      hasText: 'Kitten',
+    page.locator('.block-editor-link-control__search-item-type', {
+      hasText: 'Content: Article',
     }),
   ).toBeVisible();
+  await expect(
+    page.locator('.block-editor-link-control__search-item-type', {
+      hasText: 'Content: Page',
+    }),
+  ).toBeVisible();
+  await expect(
+    page.locator('.block-editor-link-control__search-item-type', {
+      hasText: 'Media: Remote video',
+    }),
+  ).toBeVisible();
+});
+
+test('@drupal-only linkit translations', async ({ page }) => {
+  await drupalLogin(page);
+
+  await page.goto(`${drupal.baseUrl}/en/node/add/page`);
+  await page.getByLabel('Title', { exact: true }).fill(`English page`);
+  await page.getByRole('button', { name: 'Save' }).click();
+  await page.getByRole('link', { name: 'Translate' }).click();
+  await page.click(':text-is("Add"):right-of(:text-is("German"))');
+  await page
+    .getByLabel('Titel', { exact: true })
+    .fill(`German Translation page`);
+  await page.getByRole('button', { name: 'Speichern' }).click();
+
+  await page.goto(`${drupal.baseUrl}/en/node/add/gutenberg_page`);
+  await page.click('[data-type="core/paragraph"]');
+  await page.type('[data-type="core/paragraph"]', 'Text');
+  await page.press(
+    '[data-type="core/paragraph"]',
+    os.platform() === 'darwin' ? 'Meta+a' : 'Control+a',
+  );
+  await page.click('[aria-label="Link"]');
+  await page.getByPlaceholder('Search or type url').fill('German Translation');
+
+  await expect(
+    page.locator('.block-editor-link-control__search-item-title'),
+  ).toHaveText('English page (German Translation page)');
+
+  await page.goto(`${drupal.baseUrl}/de/node/add/gutenberg_page`);
+  await page.click('[data-type="core/paragraph"]');
+  await page.type('[data-type="core/paragraph"]', 'Text');
+  await page.press(
+    '[data-type="core/paragraph"]',
+    os.platform() === 'darwin' ? 'Meta+a' : 'Control+a',
+  );
+  await page.click('[aria-label="Link"]');
+  await page
+    .getByPlaceholder('Suchen oder URL eingeben')
+    .fill('German Translation');
+
+  await expect(
+    page.locator('.block-editor-link-control__search-item-title'),
+  ).toHaveText('German Translation page');
+  await expect(
+    page.locator('.block-editor-link-control__search-item-title'),
+  ).not.toHaveText('English page');
 });
