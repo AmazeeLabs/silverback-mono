116b2bcf490c2a5d9af86899387e845fd6373890
Philipp Melab <philipp.melab@gmail.com>
2023-05-16 09:20:16 +0000

    feat(react): useLocation hook in bridge
    Expose a "useLocation" hook through @amazeelabs/bridge
    and @amazeelabs/scalars.

--- packages/npm/@amazeelabs/bridge/src/link.tsx
+++ packages/npm/@amazeelabs/bridge/src/link.tsx
@@ -16,3 +16,22 @@ export function Link(
 export function navigate(to: string) {
   window.location.href = to;
 }
+
+type Location = {
+  /**
+   * The url path.
+   */
+  pathname: string;
+  /**
+   * The query string, including '#' if not empty.
+   */
+  search: string;
+  /**
+   * The current hash, including '#' if not empty.
+   */
+  hash: string;
+}
+
+export function useLocation() : Location {
+  return window.location;
+}
