fa41672e39c2a0550c192924939283e1fb35d5d2
Christophe Jossart <christophe.jossart@amazeelabs.com>
2023-05-23 18:33:46 +0200

    refactor: use const for field name

--- packages/composer/amazeelabs/silverback_gutenberg/tests/src/Kernel/BlockValidationRuleTest.php
+++ packages/composer/amazeelabs/silverback_gutenberg/tests/src/Kernel/BlockValidationRuleTest.php
@@ -6,6 +6,8 @@ use Drupal\KernelTests\KernelTestBase;
 
 class BlockValidationRuleTest extends KernelTestBase {
 
+  const FIELD_NAME = 'field_test';
+
   protected static $modules = [
     'path_alias',
     'silverback_gutenberg',
@@ -23,18 +25,18 @@ class BlockValidationRuleTest extends KernelTestBase {
 
   public function testEmailValidator() {
     $emailValidator = $this->validatorRulePluginManager->createInstance('email');
-    $this->assertTrue($emailValidator->validate('test@example', 'field_test'));
-    $this->assertTrue($emailValidator->validate('test@example.com', 'field_test'));
-    $this->assertEquals($emailValidator->validate('test@', 'field_test'), '<em class="placeholder">field_test</em> is not valid.');
+    $this->assertTrue($emailValidator->validate('test@example', self::FIELD_NAME));
+    $this->assertTrue($emailValidator->validate('test@example.com', self::FIELD_NAME));
+    $this->assertEquals($emailValidator->validate('test@', self::FIELD_NAME), '<em class="placeholder">' . self::FIELD_NAME . '</em> is not valid.');
   }
 
   public function testRequiredValidator() {
-    $requiredMessage = '<em class="placeholder">field_test</em> field is required.';
+    $requiredMessage = '<em class="placeholder">' . self::FIELD_NAME . '</em> field is required.';
     $requiredValidator = $this->validatorRulePluginManager->createInstance('required');
-    $this->assertTrue($requiredValidator->validate('banana', 'field_test'));
-    $this->assertEquals($requiredValidator->validate(NULL, 'field_test'), $requiredMessage);
-    $this->assertEquals($requiredValidator->validate('', 'field_test'), $requiredMessage);
-    $this->assertEquals($requiredValidator->validate('_none', 'field_test'), $requiredMessage);
+    $this->assertTrue($requiredValidator->validate('banana', self::FIELD_NAME));
+    $this->assertEquals($requiredValidator->validate(NULL, self::FIELD_NAME), $requiredMessage);
+    $this->assertEquals($requiredValidator->validate('', self::FIELD_NAME), $requiredMessage);
+    $this->assertEquals($requiredValidator->validate('_none', self::FIELD_NAME), $requiredMessage);
   }
 
 }
