4787d007b8f09905930b245ece9925d371ec9f7f
Christophe Jossart <christophe.jossart@amazeelabs.com>
2023-05-23 21:37:21 +0200

    test: add field validator

--- packages/composer/amazeelabs/silverback_gutenberg/tests/src/Kernel/BlockValidationRuleTest.php
+++ packages/composer/amazeelabs/silverback_gutenberg/tests/src/Kernel/BlockValidationRuleTest.php
@@ -3,15 +3,27 @@
 namespace Drupal\Tests\silverback_gutenberg\Kernel;
 
 use Drupal\KernelTests\KernelTestBase;
+use Drupal\node\Entity\Node;
 use Drupal\silverback_gutenberg\Plugin\Validation\Constraint\Gutenberg;
 use Drupal\silverback_gutenberg\Plugin\Validation\Constraint\GutenbergValidator;
+use Drupal\Tests\node\Traits\ContentTypeCreationTrait;
+use Drupal\Tests\silverback_gutenberg\Traits\SampleAssetTrait;
+use Symfony\Component\Validator\Context\ExecutionContextInterface;
 
 class BlockValidationRuleTest extends KernelTestBase {
+
+  use ContentTypeCreationTrait;
+  use SampleAssetTrait;
 
   const FIELD_NAME = 'field_test';
 
   protected static $modules = [
+    'system',
     'path_alias',
+    'field',
+    'node',
+    'user',
+    'text',
     'silverback_gutenberg',
     'silverback_gutenberg_test_validator',
   ];
@@ -28,6 +40,16 @@ class BlockValidationRuleTest extends KernelTestBase {
 
   protected function setUp(): void {
     parent::setUp();
+
+    $this->installSchema('system', 'sequences');
+    $this->installEntitySchema('node');
+    $this->installEntitySchema('user');
+    $this->installConfig('node');
+    $this->createContentType([
+      'type' => 'page',
+      'name' => 'Basic page'
+    ]);
+
     $this->validatorRulePluginManager = \Drupal::service('plugin.manager.gutenberg_validator_rule');
     $this->validatorPluginManager = \Drupal::service('plugin.manager.gutenberg_validator');
   }
@@ -91,6 +113,82 @@ class BlockValidationRuleTest extends KernelTestBase {
       ],
     ];
     $this->assertEquals($groupValidator->validateContent($groupBlock), ['is_valid' => TRUE, 'message' => '']);
+  }
+
+  /**
+   * @dataProvider providerTestFieldValidator
+   */
+  public function testFieldValidator(string $body, array $expected_violations): void {
+    $context = $this->prophesize(ExecutionContextInterface::class);
+    $constraintValidator = new GutenbergValidator($this->validatorPluginManager, $this->validatorRulePluginManager);
+    $constraintValidator->initialize($context->reveal());
+
+    $node = Node::create([
+      'type' => 'page',
+      'title' => 'Test',
+      'body' => $body,
+    ]);
+    $node->save();
+    $constraintValidator->validate($node->get('body'), new Gutenberg());
+    $violations = $this->getViolations($constraintValidator);
+    $this->assertEquals($violations, $expected_violations);
+  }
+
+  /**
+   * Data provider for ::testFieldValidator().
+   */
+  public function providerTestFieldValidator() {
+    return [
+      [
+        'body' => '<!-- wp:custom/link {"linkLabel":"test","linkUrl":"https://example.com"} /-->',
+        'violations' => [],
+      ],
+      [
+        'body' => '<!-- wp:custom/link {"linkLabel":"test","linkUrl":""} /-->',
+        'violations' => [
+          [
+            'attribute' => 'linkUrl',
+            'blockName' => 'custom/link',
+            'rule' => 'required',
+            'message' => 'Link: <em class="placeholder">Link URL</em> field is required.',
+          ],
+        ]
+      ],
+      [
+        'body' => '<!-- wp:custom/link {"linkLabel":"","linkUrl":"https://example.com"} /-->',
+        'violations' => [
+          [
+            'attribute' => 'linkLabel',
+            'blockName' => 'custom/link',
+            'rule' => 'required',
+            'message' => 'Link: <em class="placeholder">Link Label</em> field is required.',
+          ],
+        ]
+      ],
+      [
+        'body' => '<!-- wp:custom/link {"linkLabel":"","linkUrl":""} /-->',
+        'violations' => [
+          [
+            'attribute' => 'linkUrl',
+            'blockName' => 'custom/link',
+            'rule' => 'required',
+            'message' => 'Link: <em class="placeholder">Link URL</em> field is required.',
+          ],
+          [
+            'attribute' => 'linkLabel',
+            'blockName' => 'custom/link',
+            'rule' => 'required',
+            'message' => 'Link: <em class="placeholder">Link Label</em> field is required.',
+          ],
+        ]
+      ]
+    ];
+  }
+
+  private function getViolations(GutenbergValidator $validator) {
+    $reflection = new \ReflectionClass($validator);
+    $property = $reflection->getProperty('violations');
+    return $property->getValue($validator);
   }
 
 }
