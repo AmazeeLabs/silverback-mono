112bb6efe4a5032a0b3631e76988e33a25a4bc19
Christophe Jossart <christophe.jossart@amazeelabs.com>
2023-05-23 19:35:40 +0200

    test: add content validator

--- packages/composer/amazeelabs/silverback_gutenberg/tests/src/Kernel/BlockValidationRuleTest.php
+++ packages/composer/amazeelabs/silverback_gutenberg/tests/src/Kernel/BlockValidationRuleTest.php
@@ -3,6 +3,8 @@
 namespace Drupal\Tests\silverback_gutenberg\Kernel;
 
 use Drupal\KernelTests\KernelTestBase;
+use Drupal\silverback_gutenberg\Plugin\Validation\Constraint\Gutenberg;
+use Drupal\silverback_gutenberg\Plugin\Validation\Constraint\GutenbergValidator;
 
 class BlockValidationRuleTest extends KernelTestBase {
 
@@ -11,6 +13,7 @@ class BlockValidationRuleTest extends KernelTestBase {
   protected static $modules = [
     'path_alias',
     'silverback_gutenberg',
+    'silverback_gutenberg_test_validator',
   ];
 
   /**
@@ -18,25 +21,76 @@ class BlockValidationRuleTest extends KernelTestBase {
    */
   protected $validatorRulePluginManager;
 
+  /**
+   * @var \Drupal\silverback_gutenberg\GutenbergValidation\GutenbergValidatorManager
+   */
+  protected $validatorPluginManager;
+
   protected function setUp(): void {
     parent::setUp();
     $this->validatorRulePluginManager = \Drupal::service('plugin.manager.gutenberg_validator_rule');
+    $this->validatorPluginManager = \Drupal::service('plugin.manager.gutenberg_validator');
   }
 
-  public function testEmailValidator() {
+  public function testEmailValidatorRule(): void {
+    /** @var \Drupal\silverback_gutenberg\GutenbergValidation\GutenbergValidatorRuleInterface $emailValidator */
     $emailValidator = $this->validatorRulePluginManager->createInstance('email');
     $this->assertTrue($emailValidator->validate('test@example', self::FIELD_NAME));
     $this->assertTrue($emailValidator->validate('test@example.com', self::FIELD_NAME));
     $this->assertEquals($emailValidator->validate('test@', self::FIELD_NAME), '<em class="placeholder">' . self::FIELD_NAME . '</em> is not valid.');
   }
 
-  public function testRequiredValidator() {
+  public function testRequiredValidatorRule(): void {
     $requiredMessage = '<em class="placeholder">' . self::FIELD_NAME . '</em> field is required.';
+    /** @var \Drupal\silverback_gutenberg\GutenbergValidation\GutenbergValidatorRuleInterface $requiredValidator */
     $requiredValidator = $this->validatorRulePluginManager->createInstance('required');
     $this->assertTrue($requiredValidator->validate('banana', self::FIELD_NAME));
     $this->assertEquals($requiredValidator->validate(NULL, self::FIELD_NAME), $requiredMessage);
     $this->assertEquals($requiredValidator->validate('', self::FIELD_NAME), $requiredMessage);
     $this->assertEquals($requiredValidator->validate('_none', self::FIELD_NAME), $requiredMessage);
+  }
+
+  /**
+   * Limit test to content validator only.
+   *
+   * More specific cardinality tests are covered by BlocksValidatorCardinalityTest.
+   *
+   * @return void
+   */
+  public function testContentValidator(): void {
+    // Validates any blocks.
+    /** @var \Drupal\silverback_gutenberg\GutenbergValidation\GutenbergValidatorInterface $columnValidator */
+    $columnValidator = $this->validatorPluginManager->createInstance('column_validator');
+    $columnBlock = [
+      'blockName' => 'core/column',
+      'innerBlocks' => [
+        [
+          'blockName' => 'core/paragraph',
+        ],
+        [
+          'blockName' => 'core/list',
+        ],
+      ],
+    ];
+    $this->assertEquals($columnValidator->validateContent($columnBlock), ['is_valid' => TRUE, 'message' => '']);
+
+    // Validates multiple specific blocks.
+    $groupValidator = $this->validatorPluginManager->createInstance('group_validator');
+    $groupBlock = [
+      'blockName' => 'core/group',
+      'innerBlocks' => [
+        [
+          'blockName' => 'core/paragraph',
+        ],
+        [
+          'blockName' => 'core/paragraph',
+        ],
+        [
+          'blockName' => 'core/list',
+        ],
+      ],
+    ];
+    $this->assertEquals($groupValidator->validateContent($groupBlock), ['is_valid' => TRUE, 'message' => '']);
   }
 
 }
