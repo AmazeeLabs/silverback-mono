f040d698fa6baf0995a09eabbbe9004cb4aa35bd
Philipp Melab <philipp.melab@gmail.com>
2023-05-27 05:18:57 +0000

    refactor(react): move navigate into useLocation

--- packages/npm/@amazeelabs/bridge/src/index.tsx
+++ packages/npm/@amazeelabs/bridge/src/index.tsx
@@ -1,37 +1,43 @@
 import React, { AnchorHTMLAttributes, DetailedHTMLProps } from 'react';
 
 export function Link(
-    props: DetailedHTMLProps<
-        AnchorHTMLAttributes<HTMLAnchorElement>,
-        HTMLAnchorElement
-    >,
+  props: DetailedHTMLProps<
+    AnchorHTMLAttributes<HTMLAnchorElement>,
+    HTMLAnchorElement
+  >,
 ) {
-    return (
-        <a href={props.href} className={props.className}>
-            {props.children}
-        </a>
-    );
+  return (
+    <a href={props.href} className={props.className}>
+      {props.children}
+    </a>
+  );
 }
 
-export function navigate(to: string) {
-    window.location.href = to;
-}
+export type Location = {
+  /**
+   * The url path.
+   */
+  pathname: string;
+  /**
+   * The query string, including '?' if not empty.
+   */
+  search: string;
+  /**
+   * The current hash, including '#' if not empty.
+   */
+  hash: string;
 
-type Location = {
-    /**
-     * The url path.
-     */
-    pathname: string;
-    /**
-     * The query string, including '?' if not empty.
-     */
-    search: string;
-    /**
-     * The current hash, including '#' if not empty.
-     */
-    hash: string;
-}
+  /**
+   * Change the location to a given url.
+   */
+  navigate: (to: string) => void;
+};
 
 export function useLocation(): Location | undefined {
-    return window.location;
+  return {
+    ...window.location,
+    navigate(to: string) {
+      window.location.href = to;
+    },
+  };
 }
