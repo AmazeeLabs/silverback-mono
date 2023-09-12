7c9b2453baaf96607404a9d26f257eb27b7404e2
Philipp Melab <philipp.melab@gmail.com>
2023-09-12 15:32:19 +0000

    refactor(cloudinary): remove remaining traces of the cloudinary mock worker

--- packages/npm/@amazeelabs/cloudinary-responsive-image/src/index.ts
+++ packages/npm/@amazeelabs/cloudinary-responsive-image/src/index.ts
@@ -1,3 +1,1 @@
 export { buildResponsiveImage } from './responsive_image';
-export { parseCloudinaryUrl } from './worker-lib';
-export { mockCloudinaryImage } from './worker-lib';
--- packages/npm/@amazeelabs/cloudinary-responsive-image/tsup.config.worker.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'tsup';
-
-export default defineConfig({
-  entry: ['src/mock-cloudinary-register.ts', 'src/mock-cloudinary-worker.ts'],
-  outDir: 'worker',
-  format: ['iife'],
-  legacyOutput: true,
-});
