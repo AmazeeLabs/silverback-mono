e3f2228f0f8203404a52a809ab9c29ee872ad3ae
Philipp Melab <philipp.melab@gmail.com>
2023-09-12 18:18:25 +0200

    Run integration tests

--- packages/tests/silverback-gatsby/playwright-tests/content-updates.spec.ts
+++ packages/tests/silverback-gatsby/playwright-tests/content-updates.spec.ts
@@ -7,6 +7,8 @@ import {
 } from '@amazeelabs/silverback-playwright';
 import { expect, PlaywrightTestArgs, test } from '@playwright/test';
 
+// Arbitrary change to force integration test run.
+
 test.beforeAll(async () => {
   await resetState();
 });
