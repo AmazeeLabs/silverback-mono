66ac3a98edff9d93f6045756d1222a21f88f47a1
Philipp Melab <philipp.melab@gmail.com>
2023-09-25 07:55:54 +0200

    test: e2e test case for injected directives output

--- packages/tests/silverback-gatsby/playwright-tests/gatsby-directives.spec.ts
+++ packages/tests/silverback-gatsby/playwright-tests/gatsby-directives.spec.ts
@@ -7,6 +7,15 @@ test.beforeAll(async () => {
 
 test('@gatsby-both templates', async ({ page }) => {
   await page.goto(`${gatsby.baseUrl}/contacts`);
+  await expect(
+    page.locator('p:text-is("Schema value: value from schema")'),
+  ).toBeVisible();
+  await expect(
+    page.locator('p:text-is("Parent value: parent value")'),
+  ).toBeVisible();
+  await expect(
+    page.locator('p:text-is("Argument value: argument value")'),
+  ).toBeVisible();
   await expect(page.locator('td:text-is("Frank Sinatra")')).toBeVisible();
   await expect(page.locator('td:text-is("Elvis Presley")')).toBeVisible();
   await expect(page.locator('td:text-is("John Doe")')).toBeVisible();
