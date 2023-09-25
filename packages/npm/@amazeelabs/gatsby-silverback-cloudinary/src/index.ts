5b2ee695457192a3c46fbe1e1297b187195a6b63
Philipp Melab <philipp.melab@gmail.com>
2023-09-25 11:22:16 +0200

    feat(cloudinary): expose a @responsiveImage directive for gatsby

--- packages/npm/@amazeelabs/gatsby-silverback-cloudinary/src/index.ts
+++ packages/npm/@amazeelabs/gatsby-silverback-cloudinary/src/index.ts
@@ -1,1 +1,16 @@
+import type { registerDirective } from '@amazeelabs/gatsby-source-silverback';
+
+import { resolveResponsiveImage } from './resolvers/responsive_image';
+
 export * from './resolvers/responsive_image';
+
+export function directives(register: registerDirective) {
+  register('responsiveImage', (source: any, args: any) => {
+    return resolveResponsiveImage(source, {
+      width: args.width,
+      height: args.height,
+      sizes: args.sizes,
+      transform: args.transform,
+    });
+  });
+}
