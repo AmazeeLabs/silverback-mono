da95ecc93e9407ca618af91f122573c0ab27211b
Philipp Melab <philipp.melab@gmail.com>
2023-10-05 05:44:52 +0200

    feat(graphql): reusable package for directive based resolvers

---
+++ packages/npm/@amazeelabs/graphql-directives/src/resolvers.ts
@@ -0,0 +1,110 @@
+import type { GraphQLFieldResolver, GraphQLSchema } from 'graphql';
+import { getArgumentValues, isObjectType } from 'graphql';
+import { flow, isString } from 'lodash-es';
+
+export function createResolveConfig(
+  schema: GraphQLSchema,
+  directives: Record<string, GraphQLFieldResolver<any, any>>,
+): Record<string, Record<string, GraphQLFieldResolver<any, any>>> {
+  const mapping = extractResolverMapping(schema, directives);
+  const config: Record<
+    string,
+    Record<string, GraphQLFieldResolver<any, any>>
+  > = {};
+  Object.keys(mapping).forEach((type) => {
+    if (!config[type]) {
+      config[type] = {};
+    }
+    Object.keys(mapping[type]).forEach((field) => {
+      config[type][field] = buildResolver(mapping[type][field], directives);
+    });
+  });
+  return config;
+}
+
+/**
+ * Extract fields types with @resolveBy directives.
+ *
+ * Returns a nested map of type names, field names, package and function name tuples.
+ */
+export function extractResolverMapping(
+  schema: GraphQLSchema,
+  directives: Record<string, GraphQLFieldResolver<any, any>>,
+) {
+  const resolvers = {} as Record<
+    string,
+    Record<string, Array<[string, Record<string, unknown>]>>
+  >;
+  Object.values(schema.getTypeMap()).forEach((type) => {
+    if (isObjectType(type)) {
+      Object.values(type.getFields()).forEach((field) => {
+        field.astNode?.directives?.forEach((dir) => {
+          if (directives[dir.name.value]) {
+            const directive = schema.getDirective(dir.name.value);
+            if (directive) {
+              if (!resolvers[type.name]) {
+                resolvers[type.name] = {};
+              }
+              if (!resolvers[type.name][field.name]) {
+                resolvers[type.name][field.name] = [] as Array<
+                  [string, Record<string, unknown>]
+                >;
+              }
+              resolvers[type.name][field.name].push([
+                directive.name,
+                getArgumentValues(directive, dir),
+              ]);
+            }
+          }
+        });
+      });
+    }
+  });
+  return resolvers;
+}
+
+export function buildResolver(
+  config: Array<[string, Record<string, unknown>]>,
+  directives: Record<string, Function>,
+): GraphQLFieldResolver<any, any> {
+  return (source, args, context, info) => {
+    const fns = [
+      (parent: any) => {
+        return parent?.[info?.fieldName];
+      },
+      ...config.map(([name, spec]) => {
+        return (parent: any) => {
+          return directives[name](
+            parent,
+            processDirectiveArguments(parent, args, spec),
+            context,
+            info,
+          );
+        };
+      }),
+    ] as Array<Function>;
+    const chain = flow(fns as any);
+    return chain(source);
+  };
+}
+
+export function processDirectiveArguments(
+  parent: unknown,
+  args: Record<string, any>,
+  spec: Record<string, unknown>,
+) {
+  return Object.fromEntries(
+    Object.keys(spec).map((key) => {
+      const val = spec[key];
+      if (isString(val)) {
+        if (val === '$') {
+          return [key, parent];
+        }
+        if (val.match(/^\$.+/)) {
+          return [key, args[val.substr(1)]];
+        }
+      }
+      return [key, spec[key]];
+    }),
+  );
+}
