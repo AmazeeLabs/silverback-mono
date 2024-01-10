2036d8a3cef9e5c75a9e12ea7060570771ec6913
Philipp Melab <philipp.melab@gmail.com>
2024-01-10 17:06:28 +0100

    refactor(SLB-202): implement fragment inlining

---
+++ packages/npm/@amazeelabs/codegen-operation-ids/src/inline.test.ts
@@ -0,0 +1,93 @@
+import {
+  DefinitionNode,
+  FragmentDefinitionNode,
+  Kind,
+  OperationDefinitionNode,
+  parse,
+  print,
+} from 'graphql';
+import { describe, expect, it } from 'vitest';
+
+import { inlineFragments } from './inline';
+
+function isFragmentDefinitionNode(
+  def: DefinitionNode,
+): def is FragmentDefinitionNode {
+  return def.kind === Kind.FRAGMENT_DEFINITION;
+}
+
+function isOperationDefinitionNode(
+  def: DefinitionNode,
+): def is OperationDefinitionNode {
+  return def.kind === Kind.OPERATION_DEFINITION;
+}
+
+describe('inlineFragments', () => {
+  it('inlines a fragment into a query', () => {
+    const doc = parse(`
+  query {
+      ...A
+  }
+  fragment A on Query {
+    myprop
+  }
+  `);
+    const [query] = doc.definitions.filter(isOperationDefinitionNode);
+    const [A] = doc.definitions.filter(isFragmentDefinitionNode);
+    const inlined = inlineFragments(query, new Map(Object.entries({ A })));
+    expect(print(inlined)).toEqual(`{
+  myprop
+}`);
+  });
+
+  it('inlines a fragment into a field', () => {
+    const doc = parse(`
+  query {
+    a {
+      ...A
+    }
+  }
+  fragment A on A {
+    myprop
+  }
+  `);
+    const [query] = doc.definitions.filter(isOperationDefinitionNode);
+    const [A] = doc.definitions.filter(isFragmentDefinitionNode);
+    const inlined = inlineFragments(query, new Map(Object.entries({ A })));
+    expect(print(inlined)).toEqual(`{
+  a {
+    myprop
+  }
+}`);
+  });
+
+  it('inlines nested fragments', () => {
+    const doc = parse(`query {
+  a {
+    propA
+    ...A
+  }
+}
+fragment A on A {
+  propA
+  propB {
+    ...B
+  }
+}
+fragment B on B {
+  propC
+}`);
+    const [query] = doc.definitions.filter(isOperationDefinitionNode);
+    const [A, B] = doc.definitions.filter(isFragmentDefinitionNode);
+    const inlined = inlineFragments(query, new Map(Object.entries({ A, B })));
+    expect(print(inlined)).toEqual(`{
+  a {
+    propA
+    propA
+    propB {
+      propC
+    }
+  }
+}`);
+  });
+});
---
+++ packages/npm/@amazeelabs/codegen-operation-ids/src/inline.ts
@@ -0,0 +1,33 @@
+import {
+  ExecutableDefinitionNode,
+  FieldNode,
+  FragmentDefinitionNode,
+  Kind,
+  SelectionNode,
+} from 'graphql';
+
+export function inlineFragments<
+  TNode extends ExecutableDefinitionNode | FieldNode,
+>(target: TNode, fragments: Map<string, FragmentDefinitionNode>): TNode {
+  const selections = [] as Array<SelectionNode>;
+  target.selectionSet?.selections.forEach((sel) => {
+    if (sel.kind === Kind.FRAGMENT_SPREAD) {
+      const fragment = fragments.get(sel.name.value);
+      if (fragment) {
+        inlineFragments(fragment, fragments).selectionSet.selections.forEach(
+          (sel) => {
+            selections.push(sel);
+          },
+        );
+      }
+    } else if (sel.kind === Kind.FIELD && sel.selectionSet) {
+      selections.push(inlineFragments(sel, fragments));
+    } else {
+      selections.push(sel);
+    }
+  });
+  if (target.selectionSet) {
+    target.selectionSet.selections = selections;
+  }
+  return target;
+}
