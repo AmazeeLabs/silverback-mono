b698696039ee16ef2429b5aa926094b741c0b2e3
Alex Tkachev <alex.leksat@gmail.com>
2024-02-29 01:14:39 +0400

    fix: only add language to translatable content

--- packages/composer/amazeelabs/silverback_gatsby/src/Plugin/GraphQL/Directive/SilverbackGatsbyEntityId.php
+++ packages/composer/amazeelabs/silverback_gatsby/src/Plugin/GraphQL/Directive/SilverbackGatsbyEntityId.php
@@ -2,7 +2,8 @@
 
 namespace Drupal\silverback_gatsby\Plugin\GraphQL\Directive;
 
-use Drupal\Core\Entity\TranslatableInterface;
+use Drupal\Core\Entity\ContentEntityInterface;
+use Drupal\Core\Entity\EntityInterface;
 use Drupal\graphql\GraphQL\Resolver\ResolverInterface;
 use Drupal\graphql\GraphQL\ResolverBuilder;
 use Drupal\graphql_directives\DirectiveInterface;
@@ -19,14 +20,20 @@ use Drupal\graphql_directives\DirectiveInterface;
 class SilverbackGatsbyEntityId implements  DirectiveInterface {
 
   public function buildResolver(ResolverBuilder $builder, $arguments): ResolverInterface {
-    $idResolver = $builder->produce('entity_uuid')
-      ->map('entity', $builder->fromParent());
-    $langcodeResolver = $builder->callback(
-      fn (TranslatableInterface $value) => $value->language()->getId()
-    );
-    return $builder->produce('gatsby_build_id')
-      ->map('id', $idResolver)
-      ->map('langcode', $langcodeResolver);
+    return $builder->callback(function(EntityInterface $entity) {
+      if ($entity instanceof ContentEntityInterface) {
+        $isTranslatable = FALSE;
+        if (\Drupal::hasService('content_translation.manager')) {
+          /** @var \Drupal\content_translation\ContentTranslationManagerInterface $ctm */
+          $ctm = \Drupal::service('content_translation.manager');
+          $isTranslatable = $ctm->isEnabled($entity->getEntityTypeId(), $entity->bundle());
+        }
+        return $isTranslatable
+          ? $entity->uuid() . ':' . $entity->language()->getId()
+          : $entity->uuid();
+      }
+      return $entity->id();
+    });
   }
 
 }
