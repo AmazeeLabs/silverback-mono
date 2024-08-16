11f09a80206b783bfcaa504538c32bf8c16b0326
Alex Tkachev <alex.leksat@gmail.com>
2024-08-16 15:16:40 +0400

    chore: remove explicit-function-return-type rule as it breaks linting

--- packages/npm/@amazeelabs/publisher/.eslintrc.cjs
+++ packages/npm/@amazeelabs/publisher/.eslintrc.cjs
@@ -2,7 +2,6 @@ module.exports = {
   extends: ['@amazeelabs/eslint-config'],
   root: true,
   rules: {
-    '@typescript-eslint/explicit-function-return-type': 'error',
     'promise/always-return': 'off',
     'promise/catch-or-return': 'off',
   },
