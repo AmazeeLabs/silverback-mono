004931b8f0e59c50293c20fed52bbaa52b962f56
Philipp Melab <philipp.melab@gmail.com>
2024-09-11 14:29:37 +0200

    Revert "refactor(SLB-354): simplify client-side images"
    This reverts commit 8638c36c8ceaf1e853aa1c0b6cbd6dc80239e705.

--- packages/npm/@amazeelabs/image/src/client.tsx
+++ packages/npm/@amazeelabs/image/src/client.tsx
@@ -1,16 +1,28 @@
 'use client';
+import { imageDimensionsFromData } from 'image-dimensions';
 import {
   createContext,
   forwardRef,
   PropsWithChildren,
   useContext,
+  useEffect,
+  useRef,
 } from 'react';
 
 import {
+  calculateFocusPosition,
   defaultImageSettings,
   type ImageProps,
   type ImageSettings as ImageSettingsType,
+  inferTargetDimensions,
 } from './lib.js';
+
+function base64(content: string) {
+  if (typeof btoa === 'undefined') {
+    return Buffer.from(content, 'base64').toString('binary');
+  }
+  return btoa(content);
+}
 
 const ImageSettingsContext =
   createContext<ImageSettingsType>(defaultImageSettings);
@@ -28,21 +40,69 @@ export function ImageSettings({
   );
 }
 
+async function imageDimensions(src: string) {
+  const response = await fetch(src);
+  if (!response.body) {
+    throw new Error('Failed to fetch image');
+  }
+  const buffer = await response.arrayBuffer();
+  const data = new Uint8Array(buffer);
+  const result = imageDimensionsFromData(data);
+  return result;
+}
+
+function sizerImage(width: number, height: number) {
+  const svg = `<svg xmlns="http://www.w3.org/2000/svg" width="${width}" height="${height}" viewBox="0 0 ${width} ${height}"></svg>`;
+  return `data:image/svg+xml;base64,${base64(svg)}`;
+}
+
 export const Image = forwardRef(function Image(
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  { src, focalPoint, priority, ...props }: ImageProps,
+  { focalPoint, ...props }: ImageProps,
   ref,
 ) {
-  const alterSrc = useContext(ImageSettingsContext).alterSrc || ((src) => src);
-  return (
-    <img
-      src={src ? alterSrc(src) : src}
-      ref={ref as any}
-      {...{ ...props, style: { ...props.style, objectFit: 'cover' } }}
-      loading={priority ? 'eager' : 'lazy'}
-      decoding={priority ? 'async' : 'auto'}
-      // eslint-disable-next-line react/no-unknown-property
-      fetchPriority={priority ? 'high' : 'auto'}
-    />
-  );
+  const alterSrc = useContext(ImageSettingsContext).alterSrc;
+  const imageRef = useRef<HTMLImageElement | null>(null);
+  useEffect(() => {
+    const r = imageRef || ref;
+    if (r.current && props.src) {
+      const url = alterSrc ? alterSrc(props.src) : props.src;
+      imageDimensions(url)
+        .then((source) => {
+          if (!r.current || !source) {
+            return;
+          }
+          const target = inferTargetDimensions(
+            source,
+            props.width,
+            props.height,
+          );
+          r.current.style.backgroundImage = `url(${url})`;
+          r.current.style.backgroundSize = 'cover';
+          r.current.style.maxWidth = '100%';
+
+          r.current.style.backgroundPosition = calculateFocusPosition(
+            r.current.naturalWidth,
+            r.current.naturalHeight,
+            focalPoint || [
+              r.current.naturalWidth / 2,
+              r.current.naturalHeight / 2,
+            ],
+          );
+          r.current.src = sizerImage(target.width, target.height);
+          return;
+        })
+        .catch((error) => {
+          throw error;
+        });
+    }
+  }, [
+    imageRef,
+    ref,
+    focalPoint,
+    props.width,
+    props.height,
+    props.src,
+    alterSrc,
+  ]);
+  return <img ref={imageRef || ref} {...props} />;
 });
