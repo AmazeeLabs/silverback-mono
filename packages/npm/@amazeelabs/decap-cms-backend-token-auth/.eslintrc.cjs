14620b4674fa9ea9407b903be95659b389b2c9ae
Alex Tkachev <alex.leksat@gmail.com>
2024-09-12 16:55:52 +0400

    chore: fix lint

--- packages/npm/@amazeelabs/decap-cms-backend-token-auth/.eslintrc.cjs
+++ packages/npm/@amazeelabs/decap-cms-backend-token-auth/.eslintrc.cjs
@@ -1,4 +1,9 @@
 module.exports = {
   extends: ['@amazeelabs/eslint-config'],
   root: true,
+  rules: {
+    // This rule breaks linting on Linux. We don't need it for this package
+    // anyway, so we turn it off.
+    'react/no-is-mounted': ['off'],
+  },
 };
