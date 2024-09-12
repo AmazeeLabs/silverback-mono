55870c890cbc4d39a94e6fd1cdc4198f7017eda6
Philipp Melab <philipp.melab@gmail.com>
2024-09-12 12:05:52 +0200

    chore: upgrade waku to 0.21.1 and fix regressions in bridge

--- packages/npm/@amazeelabs/bridge-waku/src/client.tsx
+++ packages/npm/@amazeelabs/bridge-waku/src/client.tsx
@@ -1,6 +1,5 @@
 'use client';
 import type { LocationProviderType, useLocationType } from '@amazeelabs/bridge';
-import { useEffect, useState } from 'react';
 import { useRouter_UNSTABLE } from 'waku';
 
 export { createLinkComponent } from './link.js';
@@ -10,19 +9,12 @@ export const createUseLocationHook: (
 ) => useLocationType = (useRouter) => () => {
   const router = useRouter();
 
-  // TODO: Use router.hash when https://github.com/dai-shi/waku/pull/746 is merged.
-  const [hash, setHash] = useState('');
-  useEffect(() => {
-    setHash(window.location.hash);
-  }, [router]);
-
   return [
     {
       pathname: router.path,
-      search: router.searchParams?.toString() || '',
-      // TODO: Remove double wrapping, based on feedback in https://github.com/dai-shi/waku/pull/746.
-      searchParams: new URLSearchParams(router.searchParams),
-      hash,
+      search: router.query,
+      searchParams: new URLSearchParams(router.query),
+      hash: router.hash,
     },
     router.push,
   ];
