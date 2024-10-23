fb55e1d606a37fc57b37ed437463ca4983b251ee
Philipp Melab <philipp.melab@gmail.com>
2024-10-23 11:23:37 +0200

    fix: properly resolve fragments nested within inline fragments

--- packages/npm/@amazeelabs/codegen-operation-ids/src/inline.ts
+++ packages/npm/@amazeelabs/codegen-operation-ids/src/inline.ts
@@ -2,12 +2,13 @@ import {
   ExecutableDefinitionNode,
   FieldNode,
   FragmentDefinitionNode,
+  InlineFragmentNode,
   Kind,
   SelectionNode,
 } from 'graphql';
 
 export function inlineFragments<
-  TNode extends ExecutableDefinitionNode | FieldNode,
+  TNode extends ExecutableDefinitionNode | FieldNode | InlineFragmentNode,
 >(node: TNode, fragments: Map<string, FragmentDefinitionNode>): TNode {
   const selections: Array<SelectionNode> = [];
   const target = structuredClone(node);
@@ -30,6 +31,22 @@ export function inlineFragments<
           },
         });
       }
+    } else if (sel.kind === Kind.INLINE_FRAGMENT) {
+      const fragmentSelections: Array<SelectionNode> = [];
+      const fragment = structuredClone(sel);
+      inlineFragments(fragment, fragments).selectionSet.selections.forEach(
+        (sel) => {
+          fragmentSelections.push(sel);
+        },
+      );
+      selections.push({
+        kind: Kind.INLINE_FRAGMENT,
+        typeCondition: sel.typeCondition,
+        selectionSet: {
+          kind: Kind.SELECTION_SET,
+          selections: fragmentSelections,
+        },
+      });
     } else if (sel.kind === Kind.FIELD && sel.selectionSet) {
       selections.push(inlineFragments(sel, fragments));
     } else {
